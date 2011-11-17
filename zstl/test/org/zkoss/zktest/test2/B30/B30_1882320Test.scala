6943ca7651cc7be9c9b6f58969f98ccb3759d535
ldnigro <ldnigro@gmail.com>
2011-11-17 10:26:33 -0300

    B30-1882320Test.scala, remove unused imports.

--- zstl/test/org/zkoss/zktest/test2/B30/B30_1882320Test.scala
+++ zstl/test/org/zkoss/zktest/test2/B30/B30_1882320Test.scala
@@ -17,18 +17,7 @@ Copyright (C) 2011 Potix Corporation. All Rights Reserved.
 package org.zkoss.zktest.test2.B30
 
 import org.zkoss.zstl.ZTL4ScalaTestCase
-import scala.collection.JavaConversions._
-import org.junit.Test
-import org.zkoss.ztl.ClientWidget
-import org.zkoss.ztl.Element
-import org.zkoss.ztl.JQuery
 import org.zkoss.ztl.Tags
-import org.zkoss.ztl.util.Scripts
-import org.zkoss.ztl.Widget
-import org.zkoss.ztl.ZK
-import org.zkoss.ztl.ZKClientTestCase
-import java.lang._
-import org.apache.velocity.texen.ant.TexenTask
 
 /**
  * A test class for bug 1882320
