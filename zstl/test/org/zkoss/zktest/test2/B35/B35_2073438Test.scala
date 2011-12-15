e3b25c3722a596036146a53a313c72159ab59381
ldnigro <ldnigro@gmail.com>
2011-12-15 01:23:02 -0300

    B35_2073438 & B35_2075728

---
+++ zstl/test/org/zkoss/zktest/test2/B35/B35_2073438Test.scala
@@ -0,0 +1,77 @@
+/* B35_2073438Test.scala
+
+{{IS_NOTE
+	Purpose:
+		
+	Description:
+		
+	History:
+		Tue Nov 08 22:51:02 GFT 2011 , Created by ldnigro
+}}IS_NOTE
+
+Copyright (C) 2011 Potix Corporation. All Rights Reserved.
+
+{{IS_RIGHT
+}}IS_RIGHT
+*/
+package org.zkoss.zktest.test2.B35
+
+import org.zkoss.zstl.ZTL4ScalaTestCase
+import org.zkoss.ztl.Tags
+import org.openqa.selenium.remote.server.handler.interactions.touch.Scroll
+
+/**
+ * A test class for bug 2073438
+ * @author ldnigro
+ *
+ */
+@Tags(tags = "B35-2073438.zul,A,E,Menu,Menupopup")
+class B35_2073438Test extends ZTL4ScalaTestCase {
+	
+  def testClick() = {
+    val zscript = """
+    		<?xml version="1.0" encoding="UTF-8"?>
+<?page title="Welcome to ZK Demo"?>
+	<!--
+		index.zul {{IS_NOTE Purpose: Description: History: Thu May 11 17:24:33
+		2006, Created by tomyeh }}IS_NOTE Copyright (C) 2006 Potix
+		Corporation. All Rights Reserved. {{IS_RIGHT }}IS_RIGHT
+	-->
+<window title="Menu Demo" border="normal" id="win">
+	<label id="label" value='Please scroll down to click the menu item "Dynamically Attribute" the window should not scroll top automatically'/>
+	<div height="1000px"> 123</div>
+	<window>
+		<menubar>
+			<menu label="Dynamically Attribute" id="dynamic">
+				<menupopup>
+				</menupopup>
+			</menu>
+		</menubar>
+	</window>
+</window>
+    """
+
+    runZTL(zscript,
+        () => {
+        	//Dynamic menu
+        	val menu=jq("$dynamic");
+          
+        	waitResponse();
+        	
+        	//Scroll down
+        	focus(menu);
+        	
+        	waitResponse();
+                  
+        	//Dynamic menu click
+        	click(menu);
+        	
+        	waitResponse();
+        	
+        	//Verify if scroll if down
+        	verifyTrue(400 >= jq("document.body.parentNode").scrollTop())
+        	        	        	        	
+        }
+    );
+   }
+}
