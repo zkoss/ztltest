17720cf4c35cd33d67cb65a5f282eacac2b08039
ldnigro <ldnigro@gmail.com>
2012-01-06 23:20:55 -0300

    B35_2096807

---
+++ zstl/test/org/zkoss/zktest/test2/B35/B35_2096807Test.scala
@@ -0,0 +1,142 @@
+/* B35_2096807Test.scala
+
+{{IS_NOTE
+	Purpose:
+		
+	Description:
+		
+	History:
+		Tue Nov 08 22:51:02 GFT 2011 , Created by ldnigro
+}}IS_NOTE
+
+Copyright (C) 2011 Potix Corporation. All Rights Reserved.
+
+{{IS_RIGHT
+}}IS_RIGHT
+*/
+package org.zkoss.zktest.test2.B35
+
+import org.zkoss.zstl.ZTL4ScalaTestCase
+import org.zkoss.ztl.Tags
+import org.zkoss.ztl.Widget
+import org.zkoss.ztl.Element
+
+
+/**
+ * A test class for bug 2096807
+ * @author ldnigro
+ *
+ */
+@Tags(tags = "B35-2096807.zul,A,E,Borderlayout,Paging")
+class B35_2096807Test extends ZTL4ScalaTestCase {
+	
+  def testClick() = {
+    val zscript = """
+<?xml version="1.0" encoding="UTF-8"?>
+
+<!--
+B35-2096807.zul
+
+{{IS_NOTE
+	Purpose:
+		
+	Description:
+		
+	History:
+		Mon Sep  8 15:21:55 TST 2008, Created by jumperchen
+}}IS_NOTE
+
+Copyright (C) 2008 Potix Corporation. All Rights Reserved.
+
+{{IS_RIGHT
+}}IS_RIGHT
+-->
+<window>
+Please click the changing paging, and then you should see the button of  the paging.
+<zscript>
+List items = new org.zkoss.zktest.test2.BigList(100); //a big list ofInteger
+</zscript>
+<borderlayout height="300px"><center flex="true">
+<listbox mold="paging">
+<listitem forEach="&#36;{items}">
+<listcell label="&#36;{each}-1"/>
+<listcell label="&#36;{each}-2"/>
+<listcell label="&#36;{each}-3"/>
+<listcell label="&#36;{each}-4"/>
+</listitem>
+</listbox>
+</center></borderlayout>
+</window>
+
+
+    """
+
+    runZTL(zscript,
+        () => {
+        	
+            waitResponse();
+            var nextd=jq(".z-paging-btn-disd .z-paging-next")
+            var next=jq(".z-paging-btn .z-paging-next")
+            
+            //Previous disabled in first page
+            var firstd=jq(".z-paging-btn-disd .z-paging-first");
+            var first=jq(".z-paging-btn .z-paging-first");
+            
+            var lastd=jq(".z-paging-btn-disd .z-paging-last");
+            var last=jq(".z-paging-btn .z-paging-last");
+            
+            var prevd=jq(".z-paging-btn-disd .z-paging-prev");
+            var prev=jq(".z-paging-btn .z-paging-prev");
+            
+            //Verify disabled and enabled pagging buttons
+            var nd=nextd.exists();
+            var n=next.exists();
+            verifyFalse(nd);
+            verifyTrue(n);
+            
+            var ld=lastd.exists();
+            var l=last.exists();
+            verifyFalse(ld);
+            verifyTrue(l);
+           
+            //first & prev are disabled 
+            var fd=firstd.exists();
+            verifyTrue(fd);
+            
+            var pd=prevd.exists();
+            verifyTrue(pd);
+            
+            //click next button
+        	click(next);
+        	waitResponse();
+        	
+        	//Verify all pagging enabled
+            verifyFalse(nextd.exists());
+            verifyTrue(next.exists());
+            
+            verifyFalse(lastd.exists());
+            verifyTrue(last.exists());
+            
+            verifyFalse(firstd.exists());
+            verifyTrue(first.exists());
+            
+            verifyFalse(prevd.exists());
+            verifyTrue(prev.exists());
+        	        	
+            click(last);
+        	waitResponse();
+        	
+        	//Verify last & next disabled
+            verifyTrue(nextd.exists());
+            verifyTrue(lastd.exists());
+            
+            verifyFalse(firstd.exists());
+            verifyTrue(first.exists());
+            
+            verifyFalse(prevd.exists());
+            verifyTrue(prev.exists());
+                   
+        }
+    );
+   }
+}
