3786adff81a868ca8fd42421e78479fc0e602e3f
pao <paowang@potix.com>
2012-02-07 10:58:50 +0800

    modify zkbind test cases for spec. changed.

--- zstl/test/org/zkoss/zktest/bind/issue/Z60_B00762Listbox1Test.scala
+++ zstl/test/org/zkoss/zktest/bind/issue/Z60_B00762Listbox1Test.scala
@@ -17,13 +17,22 @@ package org.zkoss.zktest.bind.issue
 import org.zkoss.zstl.ZTL4ScalaTestCase
 import org.zkoss.ztl.Tags
 import org.zkoss.ztl.Widget
-import java.util.ArrayList
 
 /**
  * @author pao
  */
 @Tags(tags = "zbind")
 class Z60_B00762Listbox1Test extends ZTL4ScalaTestCase {
+  def getIndex(listbox: Widget): Int = {
+    var index = -1
+    var listitems = listbox.firstChild() // include header
+    for (i <- 0 to listbox.nChildren() - 2) {
+      listitems = listitems.nextSibling();
+      if (listitems.is("selected"))
+        index = i
+    }
+    index
+  }
   def testArg() = {
     val zul = {
       <include src="/bind/issue/B00762Listbox1.zul"/>
@@ -33,86 +42,40 @@ class Z60_B00762Listbox1Test extends ZTL4ScalaTestCase {
       var outerbox = jq("$outerbox")
       var selected = jq("$selected")
       var min = jq("$min")
-      var max = jq("$max")
+      //      var max = jq("$max")
       var clean = jq("$clean")
       var select = jq("$select")
-      var reload = jq("$reload")
-      var select0 = jq("$select0")
       var showselect = jq("$showselect")
       click(outerbox.find("@listitem").eq(0).toWidget())
-      waitResponse(true)
+      waitResponse()
       verifyEquals("A", selected.toWidget().get("value"))
       click(showselect.toWidget())
-      waitResponse(true)
+      waitResponse()
       verifyEquals("0", min.toWidget().get("value"))
-      verifyEquals("0", max.toWidget().get("value"))
+      //      verifyEquals("0", max.toWidget().get("value"))
       click(outerbox.find("@listitem").eq(2).toWidget())
-      waitResponse(true)
+      waitResponse()
       verifyEquals("C", selected.toWidget().get("value"))
       click(showselect.toWidget())
-      waitResponse(true)
+      waitResponse()
       verifyEquals("2", min.toWidget().get("value"))
-      verifyEquals("2", max.toWidget().get("value"))
+      //      verifyEquals("2", max.toWidget().get("value"))
       click(clean.toWidget())
-      waitResponse(true)
-      var listitems = outerbox.toWidget().firstChild() // include header
-      var index = "-1"
-      for (i <- 0 to listitems.nChildren() - 1) {
-        listitems = listitems.nextSibling()
-        if (listitems.is("selected"))
-          index = i + ""
-      }
-      verifyEquals("-1", index);
+      waitResponse()
+      verifyEquals("-1", getIndex(outerbox.toWidget()));
       verifyEquals("", selected.toWidget().get("value"))
       click(showselect.toWidget())
-      waitResponse(true)
+      waitResponse()
       verifyEquals("-1", min.toWidget().get("value"))
-      verifyEquals("-1", max.toWidget().get("value"))
+      //      verifyEquals("-1", max.toWidget().get("value"))
       click(select.toWidget())
-      waitResponse(true)
-      listitems = outerbox.toWidget().firstChild() // include header
-      index = "-1"
-      for (i <- 0 to listitems.nChildren() - 1) {
-        listitems = listitems.nextSibling()
-        if (listitems.is("selected"))
-          index = i + ""
-      }
-      verifyEquals("1", index)
+      waitResponse()
+      verifyEquals("1", getIndex(outerbox.toWidget()))
       verifyEquals("B", selected.toWidget().get("value"))
       click(showselect.toWidget())
-      waitResponse(true)
+      waitResponse()
       verifyEquals("1", min.toWidget().get("value"))
-      verifyEquals("1", max.toWidget().get("value"))
-      click(select0.toWidget())
-      waitResponse(true)
-      listitems = outerbox.toWidget().firstChild() // include header
-      index = "-1"
-      for (i <- 0 to listitems.nChildren() - 1) {
-        listitems = listitems.nextSibling()
-        if (listitems.is("selected"))
-          index = i + ""
-      }
-      verifyEquals("0", index);
-      verifyEquals("B", selected.toWidget().get("value")) // still in B
-      click(showselect.toWidget())
-      waitResponse(true)
-      verifyEquals("0", min.toWidget().get("value"))
-      verifyEquals("0", max.toWidget().get("value"))
-      click(reload.toWidget())
-      waitResponse(true)
-      listitems = outerbox.toWidget().firstChild() // include header
-      index = "-1"
-      for (i <- 0 to listitems.nChildren() - 1) {
-        listitems = listitems.nextSibling()
-        if (listitems.is("selected"))
-          index = i + ""
-      }
-      verifyEquals("1", index);
-      verifyEquals("B", selected.toWidget().get("value"))
-      click(showselect.toWidget())
-      waitResponse(true)
-      verifyEquals("1", min.toWidget().get("value"))
-      verifyEquals("1", max.toWidget().get("value"))
+      //      verifyEquals("1", max.toWidget().get("value"))
     })
   }
 
