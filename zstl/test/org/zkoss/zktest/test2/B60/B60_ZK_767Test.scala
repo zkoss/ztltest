15122cc4958c285a890910e5a6f772372306c0f4
Bai Ben <benbai123@gmail.com>
2012-02-16 19:13:46 +0800

    add test case

---
+++ zstl/test/org/zkoss/zktest/test2/B60/B60_ZK_767Test.scala
@@ -0,0 +1,142 @@
+/* B60_ZK_767Test.scala
+
+{{IS_NOTE
+	Purpose:
+		
+	Description:
+		
+	History:
+		Thu Feb 16 18:18:53 CST 2012 , Created by benbai
+}}IS_NOTE
+
+Copyright (C) 2011 Potix Corporation. All Rights Reserved.
+
+{{IS_RIGHT
+}}IS_RIGHT
+*/
+package org.zkoss.zktest.test2.B60
+
+import org.zkoss.zstl.ZTL4ScalaTestCase
+import scala.collection.JavaConversions._
+import org.junit.Test;
+import org.zkoss.ztl.ClientWidget;
+import org.zkoss.ztl.Element;
+import org.zkoss.ztl.JQuery;
+import org.zkoss.ztl.Tags;
+import org.zkoss.ztl.util.Scripts;
+import org.zkoss.ztl.Widget;
+import org.zkoss.ztl.ZK;
+import org.zkoss.ztl.ZKClientTestCase;
+import java.lang._
+
+/**
+ * A test class for bug ZK-767
+ * @author benbai
+ *
+ */
+@Tags(tags = "B60-ZK-767.zul,A,E,Tree,Template,TreeModel,TreeSelectableModel,TreeOpenableModel")
+class B60_ZK_767Test extends ZTL4ScalaTestCase {
+	
+  def testClick() = {
+    val zscript = {
+			<zk>
+				<zscript><![CDATA[//@DECLARATION
+			       	class MyTreeNode extends org.zkoss.zul.DefaultTreeNode {
+			       		public MyTreeNode(String data, MyTreeNode[] children) {
+			      			super(data, children);
+			      		}
+			      		public MyTreeNode(String data) {
+			      			super(data);
+			      		}
+			      	}
+				]]></zscript>
+				<zscript><![CDATA[
+					MyTreeNode root = new MyTreeNode("Root", new MyTreeNode[] {});
+					String[] labs = new String[] { "A", "B", "C" };
+					for (int i = 0; i < 3; i++) {
+						MyTreeNode ni = new MyTreeNode(labs[i] + i, new MyTreeNode[] {});
+						for (int j = 0; j < 3; j++) {
+							MyTreeNode nj = new MyTreeNode(ni.getData() + "-" + j, new MyTreeNode[] {});
+							for (int k = 0; k < 2; k++)
+								nj.add(new MyTreeNode(nj.getData() + "-" + k));
+							ni.add(nj);
+						}
+						root.add(ni);
+					}
+					org.zkoss.zul.DefaultTreeModel model = new org.zkoss.zul.DefaultTreeModel(root);
+					org.zkoss.zul.DefaultTreeModel model2 = new org.zkoss.zul.DefaultTreeModel(root);
+					model.addSelectionPath(new int[] { 1, 1 });
+					model.addOpenPath(new int[] { 0, 1 });
+				]]></zscript>
+				<div>
+					<div>1. Open A0 in Tree 1. You should see A0-1 already opened.</div>
+					<div>2. Open B1 in Tree 1. You should see B1-1 already selected.</div>
+					<div>3. Click "Open 0,1", and open A0 in Tree 2. You should see A0-1 already opened.</div>
+					<div>4. Click "Select 1,1", and open B1 in Tree 2. You should see B1-1 already selected.</div>
+				</div>
+				<separator />
+				Tree 1
+				<tree id="treeOne" model="${model}" width="500px">
+					<treecols>
+						<treecol label="name"/>
+					</treecols>
+					<template name="model" >
+						<treeitem value="${each}">
+							<treerow>
+								<treecell label="${each.data}"/>
+							</treerow>
+						</treeitem>
+					</template>
+				</tree>
+				Tree 2
+				<tree id="treeTwo" model="${model2}" width="500px">
+					<treecols>
+						<treecol label="name"/>
+					</treecols>
+					<template name="model" >
+						<treeitem value="${each}">
+							<treerow>
+								<treecell label="${each.data}"/>
+							</treerow>
+						</treeitem>
+					</template>
+				</tree>
+				<hlayout>
+					<button id="btnOne" label="Open 0,1" onClick="model2.addOpenPath(new int[]{0,1})" />
+					<button id="btnTwo" label="Select 1,1" onClick="model2.addSelectionPath(new int[]{1,1})" />
+				</hlayout>
+			</zk>
+
+    }
+
+   runZTL(zscript,
+        () => {
+        var treeOne: Widget = engine.$f("treeOne");
+        var treeTwo: Widget = engine.$f("treeTwo");
+        var btnOne: Widget = engine.$f("btnOne");
+        var btnTwo: Widget = engine.$f("btnTwo");
+
+        var treeOneIcoA: Element = jq(treeOne.$n()).find(".z-treecell:contains(A0)").find(".z-tree-ico").get(0);
+        var treeOneIcoB: Element = jq(treeOne.$n()).find(".z-treecell:contains(B1)").find(".z-tree-ico").get(0);
+        var treeTwoIcoA: Element = jq(treeTwo.$n()).find(".z-treecell:contains(A0)").find(".z-tree-ico").get(0);
+        var treeTwoIcoB: Element = jq(treeTwo.$n()).find(".z-treecell:contains(B1)").find(".z-tree-ico").get(0);
+
+        click(treeOneIcoA); waitResponse();
+        verifyTrue("A0-1 of Tree 1 should already opened",
+            jq(treeOne.$n()).find(".z-treecell:contains(A0-1)").find(".z-tree-ico.z-tree-tee-open").get(0).exists());
+        click(treeOneIcoB); waitResponse();
+        verifyTrue("B1-1 of Tree 1 should already selected",
+            jq(treeOne.$n()).find(".z-treerow.z-treerow-seld:contains(B1-1)").get(0).exists());
+
+        click(btnOne); waitResponse();
+        click(treeTwoIcoA); waitResponse();
+        verifyTrue("A0-1 of Tree 1 should already opened",
+            jq(treeTwo.$n()).find(".z-treecell:contains(A0-1)").find(".z-tree-ico.z-tree-tee-open").get(0).exists());
+        click(btnTwo); waitResponse();
+        click(treeTwoIcoB); waitResponse();
+        verifyTrue("B1-1 of Tree 1 should already selected",
+            jq(treeTwo.$n()).find(".z-treerow.z-treerow-seld:contains(B1-1)").get(0).exists());
+    }
+   );
+  }
+}
---
+++ zstl/test/org/zkoss/zktest/test2/B60/B60_ZK_827Test.scala
@@ -0,0 +1,135 @@
+/* B60_ZK_827Test.scala
+
+{{IS_NOTE
+	Purpose:
+		
+	Description:
+		
+	History:
+		Thu Feb 16 09:47:46 CST 2012 , Created by benbai
+}}IS_NOTE
+
+Copyright (C) 2011 Potix Corporation. All Rights Reserved.
+
+{{IS_RIGHT
+}}IS_RIGHT
+*/
+package org.zkoss.zktest.test2.B60
+
+import org.zkoss.zstl.ZTL4ScalaTestCase
+import scala.collection.JavaConversions._
+import org.junit.Test;
+import org.zkoss.ztl.Element;
+import org.zkoss.ztl.JQuery;
+import org.zkoss.ztl.Tags;
+import org.zkoss.ztl.util.Scripts;
+import org.zkoss.ztl.Widget;
+import org.zkoss.ztl.ZK;
+import org.zkoss.ztl.ZKClientTestCase;
+import java.lang._
+
+/**
+ * A test class for bug ZK-827
+ * @author benbai
+ *
+ */
+@Tags(tags = "B60-ZK-827.zul,A,E,Listbox,ListModel,Selection,Checkmark")
+class B60_ZK_827Test extends ZTL4ScalaTestCase {
+	
+  def testClick() = {
+    val zscript = """
+			<zk>
+				<window>
+					<label multiline="true">
+					1.click sim1,sim2,sim3 to select these 3 item.
+					2.click single, you should see only one item is selected (which one is selected depends on hash code)
+					3.click another other item, the selection should change to clicked item.
+					
+					</label>
+					<zscript><![CDATA[
+					org.zkoss.zul.ListModelList lml1 = new org.zkoss.zul.ListModelList();
+					for(int i=0;i<10;i++){
+						lml1.add(""+i);
+					}
+					
+					void simbind(){
+						if(true) return;
+						//to sim zkbind sequence, you can ignore this method, just check sim1 ..etc
+						Object[] arr = lml1.getSelection().toArray();
+						lml1.clearSelection();
+						
+						for(Object obj:arr){
+							lml1.addToSelection(obj);
+						}
+					}
+					
+					void sim1(){
+						lml1.addToSelection("1");
+						lml1.clearSelection();
+						lml1.addToSelection("1");
+					}
+					void sim2(){
+						lml1.addToSelection("2");
+						lml1.clearSelection();
+						lml1.addToSelection("1");
+						lml1.addToSelection("2");
+					}
+					void sim3(){
+						lml1.addToSelection("3");
+						lml1.clearSelection();
+						lml1.addToSelection("1");
+						lml1.addToSelection("2");
+						lml1.addToSelection("3");
+					}
+					void single(){
+						listbox.setMultiple(false);
+					}
+					void showSelection(){
+						msg.setValue(""+listbox.getModel().getSelection());
+					}
+					
+					]]></zscript>
+					<listbox id="listbox" model="${lml1}" checkmark="true" multiple="true" onSelect="//simbind()">
+					
+					</listbox>
+					<button id="btnOne" label="sim1" onClick='sim1()'/>
+					<button id="btnTwo" label="sim2" onClick='sim2()'/>
+					<button id="btnThree" label="sim3" onClick='sim3()'/>
+					<button id="btnFour" label="single" onClick='single()'/>
+					<button id="btnFive" label="show selection" onClick='showSelection()'/>
+					<label id="msg" />
+				</window>
+			</zk>
+
+    """
+   runZTL(zscript,
+        () => {
+        var msg: Widget = engine.$f("msg");
+        var btnOne: Widget = engine.$f("btnOne");
+        var btnTwo: Widget = engine.$f("btnTwo");
+        var btnThree: Widget = engine.$f("btnThree");
+        var btnFour: Widget = engine.$f("btnFour");
+        var btnFive: Widget = engine.$f("btnFive");
+        def clickAndWait(wgt: org.zkoss.ztl.ClientWidget) {
+          click(wgt);
+          waitResponse();
+        }
+        clickAndWait(btnOne);
+        clickAndWait(btnTwo);
+        clickAndWait(btnThree);
+        clickAndWait(btnFour);
+        clickAndWait(btnFive);
+
+        var index: Int = Integer.parseInt(msg.$n().get("innerHTML").trim().replace("[", "").replace("]", ""));
+        verifyTrue("select should sync between client and server",
+            jq(".z-listitem-seld:contains("+index+")").exists());
+        clickAndWait(jq(".z-listitem").get(6));
+        clickAndWait(btnFive);
+        index = Integer.parseInt(msg.$n().get("innerHTML").trim().replace("[", "").replace("]", ""));
+        verifyTrue("select should sync between client and server",
+            jq(".z-listitem-seld:contains("+index+")").exists());
+        
+    }
+   );
+  }
+}
