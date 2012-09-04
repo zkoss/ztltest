1c231ef80c096c851bf67adc370f869173b82cd8
neillee <neillee@potix.com>
2012-09-04 19:34:47 +0800

    Fine tune testcases - fix timing issue - more converted testcases

---
+++ zstl_tablet/test/org/zkoss/zktest/test2/Z60/Z60_Touch_029Test.scala
@@ -0,0 +1,37 @@
+package org.zkoss.zktest.test2.Z60
+import org.zkoss.zstl.ZTL4ScalaTestCase
+import org.zkoss.ztl.Tags
+
+@Tags(tags = "Touch")
+class Z60_Touch_029Test extends ZTL4ScalaTestCase {
+	def testClick() {
+		val zscript = {
+<zk xmlns:n="native">
+	<n:h3>iPad/Android only</n:h3>
+	<div>
+		1. Click on chosenbox to show drop-down list. <separator />
+		2. Should be able to scroll the drop-down list.
+	</div>
+	<zscript><![CDATA[
+	import org.zkoss.zktest.test2.select.models.ListModelLists;
+	import org.zkoss.zul.*;
+	ListModelList model = ListModelLists.getModel(ListModelLists.MULTIPLE);
+	]]></zscript>
+	<chosenbox width="240px" model="${model}" separator=",./" />
+</zk>			
+		};
+		
+		runZTL(zscript,
+			() => {
+				singleTap(jq("@chosenbox"));
+				waitResponse(true);
+				
+				var pp = jq(".z-chosenbox-pp");
+				verifyTrue(pp.isVisible());
+				
+				var pp_scroller = pp.find("> div:last-child");
+				verifyEquals(1, pp_scroller.length());
+			}
+		);
+	}
+}
