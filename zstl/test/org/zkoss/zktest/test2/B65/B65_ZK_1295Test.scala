af6eae89daa8ee7c309bcf28593d3bb42537cb4f
HuangShengYu <noahhuang@potix.com>
2012-11-22 18:22:39 +0800

    new test case for check if button on click fn works well

---
+++ zstl/test/org/zkoss/zktest/test2/B65/B65_ZK_1295Test.scala
@@ -0,0 +1,34 @@
+package org.zkoss.zktest.test2.B65
+
+import org.zkoss.ztl.Tags
+import org.zkoss.zstl.ZTL4ScalaTestCase
+
+@Tags(tags = "B65-ZK-1295.zul")
+class B65_ZK_1295Test extends ZTL4ScalaTestCase {
+
+  def testClick() = {
+    val zscript = """<zk>
+	<html>
+		Testing Instructions:
+		<ul>
+			<li>Click on button 'b2'</li>
+		</ul>
+		
+		Expected Results:
+		<ul>
+			<li>Button 'b1' should now be focused.</li>
+		</ul>
+	</html>
+	<button id='b1' label="b1" disabled="true" />
+	<button id='b2' label="b2" onClick='b1.setDisabled(false); b1.focus();' />
+</zk>""";
+
+    runZTL(zscript,
+      () => {
+        click(jq("$b2"))
+        waitResponse()
+        verifyTrue("Button 'b1' should now be focused.", jq(".z-button:eq(0) table").hasClass("z-button-focus"))
+      })
+
+  }
+}
