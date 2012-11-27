236a9e878b6c4c6ed8e0542a3f03ba167da04172
HuangShengYu <noahhuang@potix.com>
2012-11-27 18:21:57 +0800

    new test case

---
+++ zstl/test/org/zkoss/zktest/test2/B65/B65_ZK_1407Test.scala
@@ -0,0 +1,46 @@
+
+package org.zkoss.zktest.test2.B65
+
+import org.zkoss.ztl.Tags
+import org.zkoss.zstl.ZTL4ScalaTestCase
+
+@Tags(tags = "B65-ZK-1407.zul")
+class B65_ZK_1407Test extends ZTL4ScalaTestCase {
+
+  def testClick() = {
+    val zscript = """<zk>
+                    <style>
+                      .buggy .z-combobox-inp{
+                        background-color: yellow;
+                      }
+                      .buggy.z-combobox.z-combobox-focus .z-combobox-inp{
+                        background-color: green;
+                      }
+                    </style>
+                    <label multiline="true">
+                      1. Combobox's initial background color is yellow.
+	2. Click button to open dorp-down list, and Combobox's background color will transform to green.
+	3. Select a item, and then Combobox's background color must be still green.
+                    </label>
+                    <combobox sclass="buggy">
+                      <comboitem label="item 1"></comboitem>
+                      <comboitem label="item 2"></comboitem>
+                    </combobox>
+                  </zk>"""
+
+    runZTL(zscript,
+      () => {
+        verifyEquals("Combobox's initial background color is yellow", jq(".z-combobox-inp").css("background-color"), "rgb(255, 255, 0)")
+
+        click(jq(".z-combobox-btn"))
+        waitResponse(1000)
+        verifyEquals("Combobox's initial background color is yellow", jq(".z-combobox-inp").css("background-color"), "rgb(0, 128, 0)")
+
+        click(jq("@comboitem:eq(0)"))
+        waitResponse(1000)
+        verifyEquals("Combobox's initial background color is yellow", jq(".z-combobox-inp").css("background-color"), "rgb(0, 128, 0)")
+
+      })
+
+  }
+}
---
+++ zstl/test/org/zkoss/zktest/test2/B65/B65_ZK_1419Test.scala
@@ -0,0 +1,31 @@
+
+package org.zkoss.zktest.test2.B65
+
+import org.zkoss.ztl.Tags
+import org.zkoss.zstl.ZTL4ScalaTestCase
+
+@Tags(tags = "B65-ZK-1419.zul")
+class B65_ZK_1419Test extends ZTL4ScalaTestCase {
+
+def testClick() = {
+  val zscript = <zk>
+	<label multiline="true">
+	1. Change browser language to English.
+	2. Type anything in the textbox to show errorbox.
+	3. Mouseover on the errorbox to show the tooltip text and should be "Click to re-enter data".
+	</label>
+	<textbox id="emailTextbox" width="175px" constraint="/[a-zA-Z0-9_\-.+]+@[a-zA-Z0-9_\-.+]+/: invalid e-mail" maxlength="255" />
+</zk>
+    
+  runZTL(zscript,
+    () => {
+      sendKeys(jq("@textbox"), "123123123")
+      blur(jq("@textbox"))
+      
+      mouseOver(jq(".z-errbox"))
+      waitResponse(1000)
+      verifyEquals("the tooltip text should be 'Click to re-enter data'.", jq(".z-errbox-left").attr("title"), "Click to re-enter data")
+    })
+    
+  }
+}
