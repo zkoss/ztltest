b57653b3bc3c39717eb4ac666f65f65d3cd1eed1
Huang ShengYu <noahhuang@potix.com>
2013-04-02 14:20:48 +0800

    fix 6 calendar case

--- zstl/vision-test/org/zkoss/zktest/test2/Z65/Z65_Flex_054Test.scala
+++ zstl/vision-test/org/zkoss/zktest/test2/Z65/Z65_Flex_054Test.scala
@@ -7,20 +7,20 @@ import org.zkoss.zstl.ZTL4ScalaTestCase
 @Tags(tags = "Z65-Flex-054.zul,Flex")
 class Z65_Flex_054Test extends ZTL4ScalaTestCase {
 
-def testClick() = {
-  val zscript = """<zk><hbox>
+  def testClick() = {
+    val zscript = """<zk><hbox>
     <window border="normal" height="360px"
         title="Proportional Flexibility: [Calendar, Vlayout]" width="480px">
         <vlayout height="200px" width="200px">
-            <calendar hflex="1" vflex="1"/>
-            <calendar hflex="1" vflex="2"/>
+            <calendar hflex="1" vflex="1" onCreate="self.value = new java.util.GregorianCalendar(2013, java.util.Calendar.OCTOBER, 20).getTime()" />
+            <calendar hflex="1" vflex="2" onCreate="self.value = new java.util.GregorianCalendar(2013, java.util.Calendar.OCTOBER, 20).getTime()" />
         </vlayout>
     </window>
     <window border="normal" height="360px"
         title="Proportional Flexibility: [Calendar, Vbox]" width="480px">
         <vbox height="200px" width="200px">
-            <calendar hflex="1" vflex="1"/>
-            <calendar hflex="1" vflex="2"/>
+            <calendar hflex="1" vflex="1" onCreate="self.value = new java.util.GregorianCalendar(2013, java.util.Calendar.OCTOBER, 20).getTime()" />
+            <calendar hflex="1" vflex="2" onCreate="self.value = new java.util.GregorianCalendar(2013, java.util.Calendar.OCTOBER, 20).getTime()" />
         </vbox>
     </window>
 </hbox>
@@ -28,23 +28,23 @@ def testClick() = {
     <window border="normal" height="360px"
         title="Proportional Flexibility: [Calendar, Vlayout, rounded]" width="480px">
         <vlayout height="200px" width="200px">
-            <calendar hflex="1" vflex="1"/>
-            <calendar hflex="1" vflex="2"/>
+            <calendar hflex="1" vflex="1" onCreate="self.value = new java.util.GregorianCalendar(2013, java.util.Calendar.OCTOBER, 20).getTime()" />
+            <calendar hflex="1" vflex="2" onCreate="self.value = new java.util.GregorianCalendar(2013, java.util.Calendar.OCTOBER, 20).getTime()" />
         </vlayout>
     </window>
     <window border="normal" height="360px"
         title="Proportional Flexibility: [Calendar, Vbox, rounded]" width="480px">
         <vbox height="200px" width="200px">
-            <calendar hflex="1" vflex="1"/>
-            <calendar hflex="1" vflex="2"/>
+            <calendar hflex="1" vflex="1" onCreate="self.value = new java.util.GregorianCalendar(2013, java.util.Calendar.OCTOBER, 20).getTime()" />
+            <calendar hflex="1" vflex="2" onCreate="self.value = new java.util.GregorianCalendar(2013, java.util.Calendar.OCTOBER, 20).getTime()" />
         </vbox>
     </window>
 </hbox>
-</zk>"""  
-  runZTL(zscript,
-    () => {
-      verifyImage()
-    })
-    
+</zk>"""
+    runZTL(zscript,
+      () => {
+        verifyImage()
+      })
+
   }
 }
--- zstl/vision-test/org/zkoss/zktest/test2/Z65/Z65_Flex_055Test.scala
+++ zstl/vision-test/org/zkoss/zktest/test2/Z65/Z65_Flex_055Test.scala
@@ -12,17 +12,17 @@ def testClick() = {
     <window border="normal" height="360px"
         title="minimum Flex: [Calendar, Hlayout]" width="480px">
         <hlayout vflex="min" width="200px">
-            <calendar hflex="1" vflex="1"/>
-            <calendar height="60px" hflex="1"/>
-            <calendar height="100px" hflex="1"/>
+            <calendar hflex="1" vflex="1" onCreate="self.value = new java.util.GregorianCalendar(2013, java.util.Calendar.OCTOBER, 20).getTime()" />
+            <calendar height="60px" hflex="1" onCreate="self.value = new java.util.GregorianCalendar(2013, java.util.Calendar.OCTOBER, 20).getTime()" />
+            <calendar height="100px" hflex="1" onCreate="self.value = new java.util.GregorianCalendar(2013, java.util.Calendar.OCTOBER, 20).getTime()" />
         </hlayout>
     </window>
     <window border="normal" height="360px"
         title="minimum Flex: [Calendar, Hbox]" width="480px">
         <hbox vflex="min" width="200px">
-            <calendar hflex="1" vflex="1"/>
-            <calendar height="60px" hflex="1"/>
-            <calendar height="100px" hflex="1"/>
+            <calendar hflex="1" vflex="1" onCreate="self.value = new java.util.GregorianCalendar(2013, java.util.Calendar.OCTOBER, 20).getTime()" />
+            <calendar height="60px" hflex="1" onCreate="self.value = new java.util.GregorianCalendar(2013, java.util.Calendar.OCTOBER, 20).getTime()" />
+            <calendar height="100px" hflex="1" onCreate="self.value = new java.util.GregorianCalendar(2013, java.util.Calendar.OCTOBER, 20).getTime()" />
         </hbox>
     </window>
 </hbox>
@@ -30,17 +30,17 @@ def testClick() = {
     <window border="normal" height="360px"
         title="minimum Flex: [Calendar, Hlayout, rounded]" width="480px">
         <hlayout vflex="min" width="200px">
-            <calendar hflex="1" vflex="1"/>
-            <calendar height="60px" hflex="1"/>
-            <calendar height="100px" hflex="1"/>
+            <calendar hflex="1" vflex="1" onCreate="self.value = new java.util.GregorianCalendar(2013, java.util.Calendar.OCTOBER, 20).getTime()" />
+            <calendar height="60px" hflex="1" onCreate="self.value = new java.util.GregorianCalendar(2013, java.util.Calendar.OCTOBER, 20).getTime()" />
+            <calendar height="100px" hflex="1" onCreate="self.value = new java.util.GregorianCalendar(2013, java.util.Calendar.OCTOBER, 20).getTime()" />
         </hlayout>
     </window>
     <window border="normal" height="360px"
         title="minimum Flex: [Calendar, Hbox, rounded]" width="480px">
         <hbox vflex="min" width="200px">
-            <calendar hflex="1" vflex="1"/>
-            <calendar height="60px" hflex="1"/>
-            <calendar height="100px" hflex="1"/>
+            <calendar hflex="1" vflex="1" onCreate="self.value = new java.util.GregorianCalendar(2013, java.util.Calendar.OCTOBER, 20).getTime()" />
+            <calendar height="60px" hflex="1" onCreate="self.value = new java.util.GregorianCalendar(2013, java.util.Calendar.OCTOBER, 20).getTime()" />
+            <calendar height="100px" hflex="1" onCreate="self.value = new java.util.GregorianCalendar(2013, java.util.Calendar.OCTOBER, 20).getTime()" />
         </hbox>
     </window>
 </hbox>
