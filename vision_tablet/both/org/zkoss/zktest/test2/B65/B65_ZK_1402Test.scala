b3786ddd9734ce464efd11dc79896ebba0306d19
Huang ShengYu <noahhuang@potix.com>
2013-04-03 14:39:00 +0800

    new 6 cases

---
+++ vision_tablet/both/org/zkoss/zktest/test2/B65/B65_ZK_1402Test.scala
@@ -0,0 +1,32 @@
+
+
+package org.zkoss.zktest.test2.B65
+
+import org.zkoss.ztl.Tags
+import org.zkoss.zstl.ZTL4ScalaTestCase
+import org.zkoss.ztl.Tags
+import org.zkoss.ztl.Tags
+
+@Tags(tags = "B65-ZK-1402.zul,Tablet,VisionTest")
+class B65_ZK_1402Test extends ZTL4ScalaTestCase {
+
+def testClick() = {
+  val zscript = """<zk>
+	<window title="Simple Title" border="normal" width="300px" height="300px">
+		<borderlayout>
+			<center title="Center">Center Content</center>
+			<north size="20%" title="North">North Content</north>
+			<south size="20%" title="South">South Content</south>
+			<east size="20%" title="East">East Content</east>
+			<west size="20%" title="West">West Content</west>
+		</borderlayout>
+	</window>
+</zk>
+"""  
+  runZTL(zscript,
+    () => {
+      verifyImage()
+    })
+    
+  }
+}
