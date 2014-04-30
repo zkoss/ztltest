7904930f2d988092945b04dadea37863ac12913d
Huang ShengYu <noahhuang@potix.com>
2014-04-30 11:22:24 +0800

    add test cases

---
+++ zstl/test/org/zkoss/zktest/test2/B65/B65_ZK_2231Test.scala
@@ -0,0 +1,32 @@
+package org.zkoss.zktest.test2.B65
+
+import org.zkoss.ztl.Tags
+import org.zkoss.zstl.ZTL4ScalaTestCase
+import org.junit.Test
+
+@Tags(tags = "B65-ZK-2231.zul")
+class B65_ZK_2231Test extends ZTL4ScalaTestCase {
+
+@Test
+def testClick() = {
+  val zscript = """<zk>
+	<label multiline="true">
+		Click "Upload" button, should not see any JS error.
+	</label>
+	<vbox hflex="1">
+		<button label="Upload" onClick="vbox1.setVisible(true);" />
+		<vbox hflex="1" id="vbox1" visible="false"></vbox>
+		<window title="window" visible="false" border="normal">
+			Window
+		</window>
+	</vbox>
+</zk>"""  
+  runZTL(zscript,
+    () => {
+      click(jq(".z-button"))
+      waitResponse()
+      verifyFalse("should see no javascript error", jq(".z-error").exists())
+    })
+    
+  }
+}
---
+++ zstl/vision-test/org/zkoss/zktest/test2/B65/B65_ZK_2138Test.scala
@@ -0,0 +1,48 @@
+package org.zkoss.zktest.test2.B65
+
+import org.zkoss.ztl.Tags
+import org.zkoss.zstl.ZTL4ScalaTestCase
+import org.junit.Test
+
+@Tags(tags = "B65-ZK-2138.zul")
+class B65_ZK_2138Test extends ZTL4ScalaTestCase {
+
+@Test
+def testClick() = {
+  val zscript = """<zk>
+	Open/close groupbox, the div should adjust its size
+	<window vflex="1" hflex="1" border="normal">
+		<groupbox title="groupbox">
+			<div height="100px" width="100px"
+				style="border: 1px solid red;" />
+		</groupbox>
+		<div vflex="1" style="border: 1px solid green">
+			div vflex="1"
+		</div>
+	</window>
+	<window vflex="1" hflex="1" border="normal">
+		<panel title="panel" collapsible="true" border="normal">
+			<panelchildren>
+				<div height="75px" width="100px"
+					style="border: 1px solid red;" />
+			</panelchildren>
+		</panel>
+		<div vflex="1" style="border: 1px solid green">
+			div vflex="1"
+		</div>
+	</window>
+</zk>"""  
+  runZTL(zscript,
+    () => {
+      verifyImage()
+      click(jq(".z-groupbox").toWidget().$n("title"))
+      waitResponse(true)
+      verifyImage()
+      click(jq(".z-panel").toWidget().$n("exp"))
+      waitResponse(true)
+      verifyImage()
+      
+    })
+    
+  }
+}
