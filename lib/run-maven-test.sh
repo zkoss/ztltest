38b0be4200b28ae92882622407a4a10af46de531
jerrywell <b80203@gmail.com>
2014-08-07 04:23:38 +0100

    change parameters of Ant target mtest

--- lib/run-maven-test.sh
+++ lib/run-maven-test.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-echo "params=\"$1\" \"$2\""
-mvn surefire:test -Dreport.dir="$1" -Dtest.dir="$2"
+echo "params=\"$1\" \"$2\" \"$3\" \"$4\""
+mvn surefire:test -Dreport.dir="$1" -Dtest.dir="$2" -Dbrowser="$3" -Dopenonce=$4
