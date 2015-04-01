57c2aaccdce57bf2da01d69d7689a7d09e015feb
chunfuchang <chunfuchang@potix.com>
2015-04-01 05:42:17 +0100

    add remaining 705 scala script

---
+++ zstl/test/org/zkoss/zktest/test2/B70/B70_ZK_2652Test.scala
@@ -0,0 +1,85 @@
+package org.zkoss.zktest.test2.B70
+
+import org.zkoss.ztl.Tags
+import org.zkoss.zstl.ZTL4ScalaTestCase
+import org.junit.Test
+import java.awt.event.KeyEvent
+import org.openqa.selenium.Keys
+import org.zkoss.ztl.ZKSeleneseTestBase
+
+@Tags(tags = "B70-ZK-2652.zul")
+class B70_ZK_2652Test extends ZTL4ScalaTestCase {
+
+@Test
+def testClick() = {
+  val zscript = """
+
+<?xml version="1.0" encoding="UTF-8"?>
+
+<!--
+B70-ZK-2652.zul
+
+	Purpose:
+		
+	Description:
+		
+	History:
+		Thu, Mar 12, 2015  2:58:18 PM, Created by Chunfu
+
+Copyright (C)  Potix Corporation. All Rights Reserved.
+
+-->
+<zk xmlns:ca="client/attribute">
+	<label multiline="true">
+	1. click chosenbox
+	2. scroll with mouse wheel
+	3. the popup should moved with input
+	</label>
+    <zscript>
+    ListModelList model = new ListModelList();
+    model.add(1);
+    model.add(2);
+    model.add(3);
+    </zscript>
+    
+    <window title='ca:data-scrollable="true"'   
+        contentStyle="overflow:auto" border="normal" width="500px"
+        height="300px" ca:data-scrollable="true">
+        <vlayout>
+            <html><![CDATA[
+                Steps:
+                <ol>
+                    <li>Open popup of chosenbox and combobox</li>
+                    <li>Scroll the window with mouse wheel.</li>
+                </ol>
+                
+                Results:
+                <ul>
+                    <li style="color:red">It is a bug if the popup of the chosenbox detaches, and stays in fixed position.</li>
+                    <li>This doen't happen for combobox.</li>
+                </ul>
+            ]]></html>
+
+            <chosenbox width="200px" model="${model}"/>
+            <combobox width="200px" model="${model}" />
+
+            <label forEach="1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16" value="Some Text"/>
+        </vlayout>
+    </window>
+</zk>
+    
+"""  
+  runZTL(zscript,
+    () => {
+      var chosenbox = jq("@chosenbox");
+      
+      click(chosenbox);
+      waitResponse();
+      var originTop = jq(".z-chosenbox-popup").css("top");
+      verScroll(jq("@window"), 0.7);
+      waitResponse();
+      verifyTrue(jq(".z-chosenbox-popup").css("top") < originTop);
+    })
+    
+  }
+}
---
+++ zstl/test/org/zkoss/zktest/test2/B70/B70_ZK_2656Test.scala
@@ -0,0 +1,65 @@
+package org.zkoss.zktest.test2.B70
+
+import org.zkoss.ztl.Tags
+import org.zkoss.zstl.ZTL4ScalaTestCase
+import org.junit.Test
+import java.awt.event.KeyEvent
+import org.openqa.selenium.Keys
+import org.zkoss.ztl.ZKSeleneseTestBase
+
+@Tags(tags = "B70-ZK-2656.zul")
+class B70_ZK_2656Test extends ZTL4ScalaTestCase {
+
+@Test
+def testClick() = {
+  val zscript = """
+
+<?xml version="1.0" encoding="UTF-8"?>
+
+<!--
+B70-ZK-2656.zul
+
+	Purpose:
+		
+	Description:
+		
+	History:
+		Mon, Mar 16, 2015  3:45:17 PM, Created by Chunfu
+
+Copyright (C)  Potix Corporation. All Rights Reserved.
+
+-->
+<zk xmlns:c="client">
+    <window>
+    	<label multiline="true">
+    	1. you should see zk.log shows A~H, and none of them is undefined
+    	</label>
+		<textbox></textbox>
+	    <datebox id="date" width='150px' format="dd/MM/yyyy" />
+    </window> 
+    <!-- Load the script -->
+    <script src="http://cloud.github.com/downloads/digitalBush/jquery.maskedinput/jquery.maskedinput-1.3.js" type="text/javascript"></script>
+    <script type="text/javascript">
+        zk.afterMount(function() {
+          zk.log('A', jq("@datebox input")[0]); //does not work in ZK 7
+          zk.log('B', jq("@datebox > input")[0]); //does not work in ZK 7
+          zk.log('C', jq("@datebox").find("input")[0]);  //workaround for case A using .find()
+          zk.log('D', jq("@datebox").children("input")[0]); //workaround for case B using .children()
+          zk.log('E', jq(".z-datebox input")[0]); //workaround for case A using css class
+          zk.log('F', jq(".z-datebox > input")[0]); //workaround for case B using css class
+          zk.log('G', jq("$date > input")[0]); //selection by ID also fails in ZK7
+          zk.log('H', jq("$date").children("input")[0]); //workaround for G
+        });
+    </script>
+</zk>
+
+    
+"""  
+  runZTL(zscript,
+    () => {
+      var log = jq("#zk_log").eval("val()");
+      verifyFalse(log.contains("undefined"));
+    })
+    
+  }
+}
