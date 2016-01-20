328d8d74e5f5881ada85c6b38de46e751209350e
christopher-szu <christopherszu@potix.com>
2016-01-20 12:03:27 +0800

    added test case for 8.0.1 release

---
+++ zstl/test/org/zkoss/zktest/test2/B80/B80_ZK_2901Test.scala
@@ -0,0 +1,29 @@
+package org.zkoss.zktest.test2.B80
+
+import org.junit.Test
+import org.zkoss.zstl.ZTL4ScalaTestCase
+import org.zkoss.ztl.Tags
+
+/**
+ * 
+ * @author Christopher
+ */
+@Tags(tags = "B80-ZK-2901.zul")
+class B80_ZK_2901Test extends ZTL4ScalaTestCase {
+
+@Test
+def testClick() = {
+  runZTL(
+    () => {
+      val button = jq("button")
+      // click button to start removing tree items
+      click(button)
+      // wait for at most 3 seconds, if longer, it is a bug
+      waitResponse(3000)
+      
+      val itemText = jq(".z-treecell-content .z-label").eq(0).text()
+      // check the content of the first item is "1490"
+      verifyTrue("expecting 1490, got: " + itemText, itemText.equals("1490"))
+    })
+  }
+}
