60c025df3bb06edaf49188c31bb9ef4dbd9474c1
SEFI <sefilin@potix.com>
2016-01-26 16:23:40 +0800

    added test case for 8.0.1 release

---
+++ zstl/test/org/zkoss/zktest/test2/B80/B80_ZK_3051Test.scala
@@ -0,0 +1,44 @@
+package org.zkoss.zktest.test2.B80
+
+import org.zkoss.ztl.Tags
+import org.zkoss.zstl.ZTL4ScalaTestCase
+import org.junit.Test
+import org.openqa.selenium.Dimension
+
+@Tags(tags = "B80-ZK-3051-2.zul")
+class B80_ZK_3051Test extends ZTL4ScalaTestCase {
+
+  @Test
+  def testClick() = {
+    val zscript = """
+   <include src="/test2/B80-ZK-3051-2.zul"/>
+  """
+    runZTL(zscript,
+      () => {
+        val window = driver.manage().window();
+        window.setSize(new Dimension(250, 200));
+        
+        sleep(500);
+        
+        val item = jq(".z-combobox-button");
+        click(item);
+        
+        waitResponse(true);
+        
+        val input = jq(".z-combobox");
+        val popup = jq(".z-combobox-popup");
+    		
+        val ppLeft = popup.offsetLeft();
+        val ppTop = popup.offsetTop();
+        val ppHeight = popup.outerHeight();
+        val ppWidth = popup.outerWidth();
+        val inpLeft = input.offsetLeft();
+        val inpTop = input.offsetTop();
+        val inpHeight = input.outerHeight();
+        val inpWidth = input.outerWidth();
+
+        verifyFalse((ppLeft > inpLeft - ppWidth && ppLeft < inpLeft + inpWidth) && (ppTop > inpTop - ppHeight && ppTop < inpTop + inpHeight));
+      })
+
+  }
+}
