d805c330c9bea0125cf25b348c83a25c7a38ae86
DevChu <marchuqq@gmail.com>
2016-12-30 17:14:50 +0800

    refine cases

--- zstl/test/org/zkoss/zktest/test2/B80/B80_ZK_3102Test.scala
+++ zstl/test/org/zkoss/zktest/test2/B80/B80_ZK_3102Test.scala
@@ -5,7 +5,6 @@ import org.openqa.selenium.Keys
 import org.zkoss.zstl.ZTL4ScalaTestCase
 import org.zkoss.ztl.Tags
 
-@Tags(tags = "B80-ZK-3049.zul")
 class B80_ZK_3102Test extends ZTL4ScalaTestCase {
   @Test
   def test() = {
@@ -16,14 +15,12 @@ class B80_ZK_3102Test extends ZTL4ScalaTestCase {
         sendKeys(inp, "ge")
         waitResponse()
         val pp = jq(".z-combobox-popup")
-        var distance = cb.offsetTop() - (pp.outerHeight() + pp.offsetTop())
-        verifyTrue(distance < 5 && distance >= 0)
+        verifyTolerant(cb.offsetTop(), pp.outerHeight() + pp.offsetTop(), 3)
         sendKeys(inp, Keys.BACK_SPACE)
         waitResponse()
         sendKeys(inp, Keys.BACK_SPACE)
         waitResponse()
-        distance = cb.offsetTop() - (pp.outerHeight() + pp.offsetTop())
-        verifyTrue(distance < 5 && distance >= 0)
+        verifyTolerant(cb.offsetTop(), pp.outerHeight() + pp.offsetTop(), 3)
     })
   }
 }
