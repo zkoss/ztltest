a53ec299f201c62bc86302c316393b235885c9da
Wenning Hsu <imwenninghsu@gmail.com>
2017-10-23 12:39:34 +0800

    refine test cases

---
+++ zstl/test/org/zkoss/zktest/test2/B80/B80_ZK_3077Test.scala
@@ -0,0 +1,35 @@
+package org.zkoss.zktest.test2.B80
+
+import org.junit.Test
+import org.zkoss.zstl.ZTL4ScalaTestCase
+
+/**
+  * Created by wenninghsu on 17/10/2017.
+  */
+class B80_ZK_3077Test extends ZTL4ScalaTestCase {
+
+  @Test
+  def test() = {
+    runZTL(
+      () => {
+        clickAll()
+        clickAll()
+
+        verifyTrue(jq(".z-listitem-focus").length() == 1)
+        verifyEquals(jq(".z-listitem").get(3), jq(".z-listitem-focus"))
+      }
+    )
+  }
+
+  def clickAll() = {
+    click(jq(".z-listitem-checkbox").get(0))
+    waitResponse()
+    click(jq(".z-listitem-checkbox").get(1))
+    waitResponse()
+    click(jq(".z-listitem-checkbox").get(2))
+    waitResponse()
+    click(jq(".z-listitem-checkbox").get(3))
+    waitResponse()
+  }
+
+}
