a618699d2fb22d35db62a4ebb307422c4d58cd9e
bobpeng <bobpeng@potix.com>
2017-11-07 16:49:52 +0800

    add ztl for ZK-3778

---
+++ zstl/test/org/zkoss/zktest/test2/F85/F85_ZK_3778Test.scala
@@ -0,0 +1,37 @@
+/* F85_ZK_3778Test.scala
+
+	Purpose:
+
+	Description:
+
+	History:
+		Wed, Nov 1, 2017 12:33:17 PM, Created by bobpeng
+
+Copyright (C)  Potix Corporation. All Rights Reserved.
+
+This program is distributed under LGPL Version 3.0 in the hope that
+it will be useful, but WITHOUT ANY WARRANTY.
+*/
+package org.zkoss.zktest.test2.F85
+
+import org.junit.Test
+import org.zkoss.zstl.ZTL4ScalaTestCase
+import org.zkoss.ztl.Tags;
+
+/**
+  *
+  * @author bobpeng
+  */
+@Tags(tags = "")
+class F85_ZK_3778Test extends ZTL4ScalaTestCase {
+  @Test
+  def test() = {
+    runZTL(() => {
+      dragdropTo(jq(".z-slider-button:eq(0)"), "0,0", "0,100")
+      waitResponse();
+      dragdropTo(jq(".z-slider-button:eq(1)"), "0,0", "0,100")
+      waitResponse();
+      verifyEquals("false\ntrue\n", getZKLog())
+    })
+  }
+}
