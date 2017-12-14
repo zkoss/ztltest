13196c2f2c2b3a12cd96318daa0bb4db88309e4d
Rudy Huang <rudyhuang@potix.com>
2017-12-14 09:50:41 +0800

    Add ZK-3768 test case

---
+++ zstl/test/org/zkoss/zktest/test2/B85/B85_ZK_3768Test.scala
@@ -0,0 +1,52 @@
+package org.zkoss.zktest.test2.B85
+
+import org.junit.Test
+import org.zkoss.zstl.ZTL4ScalaTestCase
+import org.zkoss.ztl.Tags
+
+/**
+  * @author rudyhuang
+  */
+@Tags(tags = "B85-ZK-3768.zul")
+class B85_ZK_3768Test extends ZTL4ScalaTestCase {
+  @Test
+  def test(): Unit = {
+    runZTL(() => {
+      click(jq("@button:eq(1)"))
+      waitResponse()
+
+      val listheader = jq("@listheader:eq(1)")
+      val width0 = listheader.outerWidth()
+      val height0 = listheader.outerHeight()
+      dragdropTo(listheader, "%d,%d".format(width0 - 2, height0 / 2), "%d,%d".format(width0 + 50, height0 / 2))
+      waitResponse()
+
+      click(jq("@button:eq(1)"))
+      waitResponse()
+
+      verifyEquals(jq("@listheader:eq(0)").width(), 150)
+    })
+  }
+
+  @Test
+  def test2(): Unit = {
+    runZTL(() => {
+      click(jq("@button:eq(1)"))
+      waitResponse()
+
+      val listheader = jq("@listheader:eq(1)")
+      val width0 = listheader.outerWidth()
+      val height0 = listheader.outerHeight()
+      dragdropTo(listheader, "%d,%d".format(width0 - 2, height0 / 2), "%d,%d".format(width0 + 50, height0 / 2))
+      waitResponse()
+
+      click(jq("@button:eq(0)"))
+      waitResponse()
+
+      click(jq("@button:eq(1)"))
+      waitResponse()
+
+      verifyEquals(jq("@listheader:eq(0)").width(), 150)
+    })
+  }
+}
