07af96548edf8354a1349c360ba6a9e773b61509
Rudy Huang <rudyhuang@potix.com>
2017-12-20 17:01:56 +0800

    Add ZK-3718 test case

---
+++ zstl/test/org/zkoss/zktest/test2/B85/B85_ZK_3718Test.scala
@@ -0,0 +1,20 @@
+package org.zkoss.zktest.test2.B85
+
+import org.junit.Test
+import org.zkoss.zstl.ZTL4ScalaTestCase
+import org.zkoss.ztl.Tags
+
+/**
+  * @author rudyhuang
+  */
+@Tags(tags = "B85-ZK-3718.zul")
+class B85_ZK_3718Test extends ZTL4ScalaTestCase {
+  @Test
+  def test(): Unit = {
+    runZTL(() => {
+      val height = jq("@vlayout").height() // 500px
+      val lastDiv = jq(".z-vlayout-inner:last")
+      verifyTolerant(height, lastDiv.offsetTop() + lastDiv.height(), 3)
+    })
+  }
+}
