631adf37c9f972d6160cf523b675179ccc3562c4
Rudy Huang <rudyhuang@potix.com>
2018-01-12 14:48:26 +0800

    Add ZK-3800 test case

---
+++ zstl/test/org/zkoss/zktest/test2/B85/B85_ZK_3800Test.scala
@@ -0,0 +1,21 @@
+package org.zkoss.zktest.test2.B85
+
+import org.junit.Test
+import org.zkoss.zstl.ZTL4ScalaTestCase
+import org.zkoss.ztl.Tags
+
+/**
+  * @author rudyhuang
+  */
+@Tags(tags = "B85-ZK-3800.zul")
+class B85_ZK_3800Test extends ZTL4ScalaTestCase {
+  @Test
+  def test(): Unit = {
+    runZTL(() => {
+      click(jq("$btnCheck"))
+      waitResponse()
+
+      verifyEquals("The height of buttons are not the same.", "true", jq("$result").html())
+    })
+  }
+}
