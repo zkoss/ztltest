b108e84f32857becd37bf6079b3974064d719c6b
Rudy Huang <rudyhuang@potix.com>
2018-01-12 14:52:36 +0800

    Add ZK-3579 test case

---
+++ zstl/test/org/zkoss/zktest/test2/B85/B85_ZK_3579Test.scala
@@ -0,0 +1,18 @@
+package org.zkoss.zktest.test2.B85
+
+import org.junit.Test
+import org.zkoss.zstl.ZTL4ScalaTestCase
+import org.zkoss.ztl.Tags
+
+/**
+  * @author rudyhuang
+  */
+@Tags(tags = "B85-ZK-3579.zul")
+class B85_ZK_3579Test extends ZTL4ScalaTestCase {
+  @Test
+  def test(): Unit = {
+    runZTL(() => {
+      verifyTolerant(400, jq("@combobutton:eq(0) .z-combobutton-content").outerWidth(), 4)
+    })
+  }
+}
