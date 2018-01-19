58f3802dc73258b645e0a7910a39c85530bc687a
Rudy Huang <rudyhuang@potix.com>
2018-01-19 19:27:11 +0800

    Add ZK-3840 test case

---
+++ zstl/test/org/zkoss/zktest/test2/B85/B85_ZK_3840Test.scala
@@ -0,0 +1,35 @@
+package org.zkoss.zktest.test2.B85
+
+import org.junit.Test
+import org.zkoss.zstl.ZTL4ScalaTestCase
+import org.zkoss.ztl.Tags
+
+/**
+  * @author rudyhuang
+  */
+@Tags(tags = "B85-ZK-3840.zul")
+class B85_ZK_3840Test extends ZTL4ScalaTestCase {
+  @Test
+  def test(): Unit = {
+    runZTL(() => {
+      val win = widget(jq("@window"))
+      val btns = jq("@button")
+      val div1 = jq("$div1")
+      val w1 = div1.width()
+
+      click(btns.eq(0))
+      waitResponse()
+      click(btns.eq(2))
+      waitResponse()
+      val w2 = div1.width()
+      verifyTrue("The width of div1 should be smaller", w2 < w1)
+
+      click(btns.eq(1))
+      waitResponse()
+      click(btns.eq(3))
+      waitResponse()
+      val w3 = div1.width()
+      verifyTrue("The width of div1 should be much smaller", w3 < w2)
+    })
+  }
+}
