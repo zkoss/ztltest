04245462af22ec4f682803298a4f11fed8800954
DevChu <marchuqq@gmail.com>
2018-02-06 18:27:58 +0800

    refine test cases

---
+++ zstl/test/org/zkoss/zktest/test2/B85/B50_2976323Test.scala
@@ -0,0 +1,26 @@
+package org.zkoss.zktest.test2.B85
+
+import org.junit.Test
+import org.zkoss.zstl.ZTL4ScalaTestCase
+import org.zkoss.ztl.Tags
+
+class B50_2976323Test extends ZTL4ScalaTestCase {
+  @Test
+  def test() = {
+    runZTL(() => {
+      var one = jq("$one")
+      var two = jq("$two")
+      var mb = jq("$mb")
+      dragdropToObject(two, one, "0,10", "0,10")
+      waitResponse()
+      verifyEquals("two", mb.first.toWidget.firstChild().get("id"))
+      verifyEquals(two.parent().get(0).firstChild().get("id"), two.get(0).get("id"))
+
+
+      dragdropToObject(one, two, "0,10", "0,10")
+      waitResponse()
+      verifyEquals("one", mb.first.toWidget.firstChild().get("id"))
+      verifyEquals(two.parent().get(0).firstChild().nextSibling().get("id"), two.get(0).get("id"))
+    })
+  }
+}
