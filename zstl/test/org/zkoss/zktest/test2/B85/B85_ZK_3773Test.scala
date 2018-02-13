84527587d344ea755201f019004d601a15ffb991
bobpeng <bobpeng@potix.com>
2018-02-13 18:11:10 +0800

    refine test case

--- zstl/test/org/zkoss/zktest/test2/B85/B85_ZK_3773Test.scala
+++ zstl/test/org/zkoss/zktest/test2/B85/B85_ZK_3773Test.scala
@@ -14,11 +14,14 @@ class B85_ZK_3773Test extends ZTL4ScalaTestCase {
     runZTL(() => {
       val themes = jq("@groupbox @button")
       0 until themes.length() foreach { i => testInTheme(themes.eq(i)) }
+      click(jq("$restore"))
+      sleep(2000)
     })
   }
 
   def testInTheme(themeBtn: ClientWidget): Unit = {
     click(themeBtn)
+    sleep(2000)
     waitResponse()
 
     waitForPageToLoad("5000")
@@ -29,7 +32,7 @@ class B85_ZK_3773Test extends ZTL4ScalaTestCase {
     val msgboxbtns = jq(".z-messagebox-button")
     val leftBtnOffsetRight = msgboxbtns.eq(0).offsetLeft() + msgboxbtns.eq(0).outerWidth(false)
     val rightBtnOffsetLeft = msgboxbtns.eq(1).offsetLeft()
-    verifyTrue("The buttons must have some spaces",  (rightBtnOffsetLeft - leftBtnOffsetRight) > 3)
+    verifyTrue("The buttons must have some spaces",  (rightBtnOffsetLeft - leftBtnOffsetRight) >= 3)
     verifyEquals("The buttons should be centered", "center", jq(".z-messagebox-buttons").css("text-align"))
 
     click(msgboxbtns.eq(0))
