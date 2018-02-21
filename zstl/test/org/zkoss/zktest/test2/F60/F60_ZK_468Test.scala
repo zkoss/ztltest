f45450f939135785aac02047c6fc8f8a9d29c3ce
DevChu <marchuqq@gmail.com>
2018-02-21 18:32:04 +0800

    refine test cases

--- zstl/test/org/zkoss/zktest/test2/F60/F60_ZK_468Test.scala
+++ zstl/test/org/zkoss/zktest/test2/F60/F60_ZK_468Test.scala
@@ -38,78 +38,21 @@ class F60_ZK_468Test extends ZTL4ScalaTestCase {
 	
   @Test
   def testClick() = {
-    val zscript = """
-			<?init class="org.zkoss.zkplus.databind.AnnotateDataBinderInit"?>
-			<zk>
-			        <zscript>
-			                <![CDATA[
-			 
-			        public class MyUserBean{
-			                private String[] userList = { "Tony", "Ryan", "Jumper", "Wing", "Sam" };
-			                private int index = 0;
-			                public ListModelList getUserList(){
-			                        return new ListModelList(Arrays.asList(userList));
-			                }
-			                public void setUserList(){
-			                }
-			                public void setIndex(int ind){
-			                        index=ind;
-			                }
-			                public int getIndex(){
-			                        return index;
-			                }
-			                public String getCurrentName(){
-			                        return userList[index];
-			                }
-			        }
-			         
-			        MyUserBean mybean = new MyUserBean();
-			 
-			        org.zkoss.zul.ItemRenderer render = new org.zkoss.zul.ItemRenderer() {
-			 
-			                public String render(Component owner, Object data) throws Exception {
-			                        return data.toString();
-			                }
-			        };
-			]]></zscript>
-			 Please select each of the item in the dropdown list and the label below should be changed as your selection 
-			        <div>
-    				<label id="outer" value="outer" />
-			        Select User:
-			        <selectbox id="box" model="@{mybean.userList}"
-			                selectedIndex="@{mybean.index}">
-			                <template name="model">
-			                ${each}
-			                </template>
-			        </selectbox>
-			 
-			        </div>
-			        Selected:
-			        <label id="val" value="@{mybean.currentName }" />
-			</zk>
-
-    """
-
-    runZTL(zscript,
-        () => {
-        var outer: Widget = engine.$f("outer");
-        var box: Widget = engine.$f("box");
-        var _val: Widget = engine.$f("val");
-
+    runZTL(() => {
+        var box: Widget = engine.$f("box")
+        var _val: Widget = engine.$f("val")
         def selectItem (content: String) {
-          select(box, content);
+          select(box, content)
           waitResponse()
-          click(outer);
-          waitResponse();
           verifyTrue("the label below should be changed as your selection",
               _val.$n().get("innerHTML").equals(content));
         }
-        selectItem("Tony");
-        selectItem("Ryan");
-        selectItem("Jumper");
-        selectItem("Wing");
-        selectItem("Sam");
+        selectItem("Tony")
+        selectItem("Ryan")
+        selectItem("Jumper")
+        selectItem("Wing")
+        selectItem("Sam")
     }
-   );
+   )
   }
 }
