5e6b0cd3e57f8e6b888512e4d393a4311957aa3a
DevChu <marchuqq@gmail.com>
2018-02-23 10:06:01 +0800

    upgrade scala version to 2.12.4

--- zstl/test/org/zkoss/zktest/test2/B30/B30_1871684Test.scala
+++ zstl/test/org/zkoss/zktest/test2/B30/B30_1871684Test.scala
@@ -1,11 +1,13 @@
 package org.zkoss.zktest.test2.B30
 
+import org.junit.Test
 import org.zkoss.zstl.ZTL4ScalaTestCase
 import org.zkoss.ztl.Tags
 
 @Tags(tags = "B30-1871684.zul,Tree")
 class B30_1871684Test extends ZTL4ScalaTestCase {
-  def test(): Unit = {
+  @Test
+  def testClick() = {
     runZTL(() => {
       click(jq("@button"))
       waitResponse()
