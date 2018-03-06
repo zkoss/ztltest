6a2086b13507b57de0474fbc988e80c4676731f9
klyvechen <klyvechen@potix.com>
2018-03-06 18:06:40 +0800

    Add ZK-3731 test case

---
+++ zstl/test/org/zkoss/zktest/test2/B85/B85_ZK_3731Test.scala
@@ -0,0 +1,31 @@
+package org.zkoss.zktest.test2.B85
+
+import org.junit.Test
+import org.zkoss.zstl.ZTL4ScalaTestCase
+import org.zkoss.ztl.Tags
+
+/* B85_ZK_3731Test.java
+
+        Purpose:
+                
+        Description:
+                
+        History:
+                Tue Mar 06 5:39 PM:09 CST 2018, Created by klyve
+
+Copyright (C) 2018 Potix Corporation. All Rights Reserved.
+*/
+@Tags(tags = "")
+class B85_ZK_3731Test extends ZTL4ScalaTestCase  {
+  @Test
+  def test(): Unit = {
+    runZTL(executor = () => {
+      for (index <- 0 to jq("a").length() - 1) {
+        click(jq("a").get(index));
+        waitResponse();
+      }
+      var number = driver.getWindowHandles.size();
+      verifyEquals(number,10);
+    })
+  }
+}
