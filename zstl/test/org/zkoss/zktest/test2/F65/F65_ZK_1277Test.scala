86e1b7f78c77bddd8c54096f3ad9fe3912b1157d
DevChu <marchuqq@gmail.com>
2018-03-06 10:19:48 +0800

    refine test cases

--- zstl/test/org/zkoss/zktest/test2/F65/F65_ZK_1277Test.scala
+++ zstl/test/org/zkoss/zktest/test2/F65/F65_ZK_1277Test.scala
@@ -12,7 +12,7 @@ class F65_ZK_1277Test extends ZTL4ScalaTestCase {
     runZTL(() => {
         val verifyHeight = (result: Int) =>
           List(".z-row", ".z-groupfoot") foreach { cls =>
-            verifyEquals("each row should be the same height", jq(cls).outerHeight(), result)
+            verifyTolerant(jq(cls).outerHeight(), result, 3) //due to new theme border
           }
         verifyHeight(jq(".z-group").outerHeight())
         click(jq(".z-button:contains(Autopaging)"))
