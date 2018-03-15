7a7d0786972ecdf457fc323c89ce00298658ad22
ZiJia <zga12181991@gmail.com>
2018-03-15 12:55:40 +0800

    Add ZK-3583 test case

---
+++ zstl/test/org/zkoss/zktest/test2/B85/B85_ZK_3583Test.scala
@@ -0,0 +1,37 @@
+/* B85_ZK_3583Test.java
+
+        Purpose:
+
+        Description:
+
+        History:
+                Thu Mar 15 12:32:39 CST 2018, Created by charlesqiu
+
+Copyright (C) 2018 Potix Corporation. All Rights Reserved.
+*/
+package org.zkoss.zktest.test2.B85
+
+import org.junit.Test
+import org.zkoss.zstl.ZTL4ScalaTestCase
+
+class B85_ZK_3583Test extends ZTL4ScalaTestCase {
+
+	@Test
+	def test() = {
+		runZTL(() => {
+			val popup = jq(".z-popup")
+
+			verifyFalse(popup.exists())
+
+			mouseOver(jq(".z-div").eq(1))
+			waitResponse(true)
+
+			verifyTrue(popup.exists())
+
+			mouseOver(jq(".z-button"))
+			waitResponse(true)
+
+			verifyTrue(popup.css("display").equals("block"))
+		})
+	}
+}
