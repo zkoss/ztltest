558e1d2f02c294671a8a31952e0de0e51b32ff7e
Rudy Huang <rudyhuang@potix.com>
2018-03-15 18:51:04 +0800

    Add ZK-3897 test case

---
+++ zstl/test/org/zkoss/zktest/test2/B85/B85_ZK_3897Test.scala
@@ -0,0 +1,52 @@
+package org.zkoss.zktest.test2.B85
+
+import org.junit.Test
+import org.openqa.selenium.Keys
+import org.zkoss.zstl.ZTL4ScalaTestCase
+import org.zkoss.ztl.Tags
+
+/**
+  * @author rudyhuang
+  */
+@Tags(tags = "B85-ZK-3897.zul")
+class B85_ZK_3897Test extends ZTL4ScalaTestCase {
+  @Test
+  def test(): Unit = {
+    runZTL(() => {
+      val timeZones = Array(jq("@button:eq(0)"), jq("@button:eq(1)"), jq("@button:eq(2)"))
+      timeZones.foreach(tz => {
+        click(tz)
+        waitResponse()
+        // 1899/12/31 23:59:59 and 2100/01/01 00:00:00 are not allowed
+        testKeyboardSelectRange("1900/01/01 23:59:59", "2099/12/31 00:00:00")
+      })
+    })
+  }
+
+  def testKeyboardSelectRange(expectMin: String, expectMax: String): Unit = {
+    val btn = jq("@datebox a.z-datebox-button")
+    val real = jq("@datebox .z-datebox-input")
+
+    click(jq("@button:contains(Near Min)"))
+    waitResponse()
+    click(btn)
+    waitResponse()
+    click(real)
+    sendKeys(real, Keys.ARROW_LEFT)
+    sendKeys(real, Keys.ARROW_LEFT)
+    click(jq(".z-calendar-selected"))
+    waitResponse()
+    verifyEquals(expectMin, real.`val`())
+
+    click(jq("@button:contains(Near Max)"))
+    waitResponse()
+    click(btn)
+    waitResponse()
+    click(real)
+    sendKeys(real, Keys.ARROW_RIGHT)
+    sendKeys(real, Keys.ARROW_RIGHT)
+    click(jq(".z-calendar-selected"))
+    waitResponse()
+    verifyEquals(expectMax, real.`val`())
+  }
+}
