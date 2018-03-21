b70718aa4da9f4eaf36f0c3dea69e9f544597aea
ZiJia <zga12181991@gmail.com>
2018-03-21 17:22:49 +0800

    Add ZK-3858 test case

---
+++ zstl/test/org/zkoss/zktest/test2/B85/B85_ZK_3858Test.scala
@@ -0,0 +1,26 @@
+/* B85_ZK_3858.java
+
+        Purpose:
+
+        Description:
+
+        History:
+                Wed Mar 21 14:32:59 CST 2018, Created by charlesqiu
+
+Copyright (C) 2018 Potix Corporation. All Rights Reserved.
+*/
+package org.zkoss.zktest.test2.B85
+
+import org.junit.Test
+import org.zkoss.zstl.ZTL4ScalaTestCase
+
+class B85_ZK_3858Test extends ZTL4ScalaTestCase {
+	@Test
+	def test() = {
+		runZTL(() => {
+			click(jq(".z-button"))
+			waitResponse(true)
+			verifyEquals(jq(".z-north-body > div").width(), jq(".z-center-body > div").width())
+		})
+	}
+}
