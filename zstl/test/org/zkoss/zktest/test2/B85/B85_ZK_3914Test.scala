91a779ff04c36a4a9fa9f372fa54b6bde4a4e972
ZiJia <zga12181991@gmail.com>
2018-03-30 16:00:35 +0800

    Add ZK-3914 test case

---
+++ zstl/test/org/zkoss/zktest/test2/B85/B85_ZK_3914Test.scala
@@ -0,0 +1,30 @@
+/* B85_ZK_3914Test.java
+
+        Purpose:
+
+        Description:
+
+        History:
+                Fri Mar 30 15:46:59 CST 2018, Created by charlesqiu
+
+Copyright (C) 2018 Potix Corporation. All Rights Reserved.
+*/
+package org.zkoss.zktest.test2.B85
+
+import org.junit.Test
+import org.zkoss.zstl.ZTL4ScalaTestCase
+import org.zkoss.ztl.JQuery
+
+class B85_ZK_3914Test extends ZTL4ScalaTestCase {
+	@Test
+	def test(): Unit = {
+		runZTL(() => {
+			testEmptyMessageWidth(jq(".z-grid-emptybody"))
+			testEmptyMessageWidth(jq(".z-listbox-emptybody"))
+		})
+	}
+
+	def testEmptyMessageWidth(emptyBody: JQuery): Unit = {
+		verifyTolerant(emptyBody.width(), emptyBody.find("td").width(), 2)
+	}
+}
