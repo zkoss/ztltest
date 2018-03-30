8aab4f04adbaea04d580f13feb6bc96592466998
ZiJia <zga12181991@gmail.com>
2018-03-30 17:34:57 +0800

    Add ZK-3635 test case

---
+++ zstl/test/org/zkoss/zktest/test2/B85/B85_ZK_3635Test.scala
@@ -0,0 +1,40 @@
+/* B85_ZK_3635Test.java
+
+        Purpose:
+
+        Description:
+
+        History:
+                Fri Mar 30 17:16:45 CST 2018, Created by charlesqiu
+
+Copyright (C) 2018 Potix Corporation. All Rights Reserved.
+*/
+package org.zkoss.zktest.test2.B85
+
+import org.junit.Test
+import org.zkoss.zstl.ZTL4ScalaTestCase
+
+class B85_ZK_3635Test extends ZTL4ScalaTestCase {
+
+	@Test
+	def test(): Unit = {
+		runZTL(() => {
+			click(jq(".z-bandbox-button"))
+			waitResponse(true)
+
+			click(jq(".z-textbox").eq(0))
+			waitResponse(true)
+
+			pressTab()
+			pressTab()
+
+			verifyTrue(jq(".z-bandpopup").exists())
+			verifyTrue(jq(".z-textbox").eq(1).is(":focus"))
+		})
+	}
+
+	def pressTab(): Unit = {
+		keyPressNative("9")
+		waitResponse(true)
+	}
+}
