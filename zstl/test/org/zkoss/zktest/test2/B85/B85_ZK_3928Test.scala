fa9b3146b55a2f4feecaa83124dddf4af9887a57
Rudy Huang <rudyhuang@potix.com>
2018-06-01 17:02:49 +0800

    Add ZK-3928 test case

---
+++ zstl/test/org/zkoss/zktest/test2/B85/B85_ZK_3928Test.scala
@@ -0,0 +1,25 @@
+package org.zkoss.zktest.test2.B85
+
+import org.junit.Test
+import org.zkoss.zstl.ZTL4ScalaTestCase
+import org.zkoss.ztl.Tags
+
+/**
+  * @author rudyhuang
+  */
+@Tags(tags = "B85-ZK-3928.zul")
+class B85_ZK_3928Test extends ZTL4ScalaTestCase {
+  @Test
+  def test(): Unit = {
+    runZTL(() => {
+      click(jq("@button:contains(Open item-0)"))
+      waitResponse()
+
+      click(jq("@button:contains(Select item-0-0-1)"))
+      waitResponse()
+
+      val scrollTop = jq("@tree .z-tree-body").scrollTop()
+      verifyEquals("The scroll position changes unnecessarily", 0, scrollTop)
+    })
+  }
+}
