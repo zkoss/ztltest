4c219fca21c15a4b8c630c226b66f239041a48da
Rudy Huang <rudyhuang@potix.com>
2018-06-12 19:21:06 +0800

    Add ZK-3943 and ZK-3766 test case

---
+++ zstl/test/org/zkoss/zktest/test2/B85/B85_ZK_3766Test.scala
@@ -0,0 +1,26 @@
+package org.zkoss.zktest.test2.B85
+
+import org.junit.Test
+import org.zkoss.zstl.ZTL4ScalaTestCase
+import org.zkoss.ztl.Tags
+
+/**
+  * @author rudyhuang
+  */
+@Tags(tags = "B85-ZK-3766.zul")
+class B85_ZK_3766Test extends ZTL4ScalaTestCase {
+  @Test
+  def test(): Unit = {
+    runZTL(() => {
+      val bandbox = jq("$target")
+      click(bandbox.find("a"))
+      waitResponse(true)
+
+      val ppLeft = jq(".z-bandbox-popup.z-bandbox-open:visible").offsetLeft()
+      val bbLeft = bandbox.offsetLeft()
+      val delta = ppLeft - bbLeft
+      val tolerance = -5
+      verifyTrue("The popup should be positioned after_end, not after_start.", delta < tolerance)
+    })
+  }
+}
---
+++ zstl/test/org/zkoss/zktest/test2/B85/B85_ZK_3943Test.scala
@@ -0,0 +1,33 @@
+package org.zkoss.zktest.test2.B85
+
+import org.junit.Test
+import org.zkoss.zstl.ZTL4ScalaTestCase
+import org.zkoss.ztl.{JQuery, Tags}
+
+/**
+  * @author rudyhuang
+  */
+@Tags(tags = "B85-ZK-3943.zul")
+class B85_ZK_3943Test extends ZTL4ScalaTestCase {
+  @Test
+  def test(): Unit = {
+    runZTL(() => {
+      click(jq("@button:eq(0)"))
+      waitResponse()
+      checkPopupPosition(jq("@button:eq(0)"), jq("$mypop"))
+
+      click(jq("@button:eq(1)"))
+      waitResponse()
+      checkPopupPosition(jq("@button:eq(1)"), jq("$mypop"))
+
+      windowResizeTo(1000, 800)
+      checkPopupPosition(jq("@button:eq(1)"), jq("$mypop"))
+    })
+  }
+
+  def checkPopupPosition(btn: JQuery, pp: JQuery): Unit = {
+    val popRightEdge = pp.offsetLeft() + pp.outerWidth()
+    val btnRightEdge = btn.offsetLeft() + btn.outerWidth()
+    verifyTolerant(popRightEdge, btnRightEdge, 2)
+  }
+}
