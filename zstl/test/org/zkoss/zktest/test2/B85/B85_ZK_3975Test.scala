d67b7ae4aee0d8cadcf393f6f1afe3fd34a4d6e5
klyvechen <klyvechen@potix.com>
2018-07-04 16:07:49 +0800

    Add ZK-3975 testcase

---
+++ zstl/test/org/zkoss/zktest/test2/B85/B85_ZK_3975Test.scala
@@ -0,0 +1,27 @@
+package org.zkoss.zktest.test2.B85
+
+import org.zkoss.zstl.ZTL4ScalaTestCase
+
+/* B85_ZK_3975Test.java
+
+        Purpose:
+                
+        Description:
+                
+        History:
+                Wed Jul 04 15:47:04 CST 2018, Created by klyve
+
+Copyright (C) 2018 Potix Corporation. All Rights Reserved.
+*/ class B85_ZK_3975Test extends ZTL4ScalaTestCase {
+  def test(): Unit = {
+    runZTL(() => {
+      var listbox = jq("@listbox")
+      verScroll(listbox, 1)
+      waitResponse()
+      click(jq(".z-listgroup-icon-open").eq(2))
+      waitResponse()
+      var lastListgroup = jq(".z-listgroup").eq(2);
+      verifyTolerant(listbox.height() + listbox.offsetTop(), lastListgroup.offsetTop() + lastListgroup.height(), 1)
+    })
+  }
+}
