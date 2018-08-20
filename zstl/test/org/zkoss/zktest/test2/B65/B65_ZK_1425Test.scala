350d2d89676de0af878977f66573934f21aaab66
Rudy Huang <rudyhuang@potix.com>
2018-08-20 12:08:34 +0800

    Add ZK-4010, ZK-4011 test cases Refine ZK-1425 test case

--- zstl/test/org/zkoss/zktest/test2/B65/B65_ZK_1425Test.scala
+++ zstl/test/org/zkoss/zktest/test2/B65/B65_ZK_1425Test.scala
@@ -9,57 +9,19 @@ class B65_ZK_1425Test extends ZTL4ScalaTestCase {
 
   @Test
   def testClick() = {
-    val zscript =
-      """<zk>
-                    <listbox id="box" width="400px">
-                      <listhead sizable="true">
-                        <listheader id="name" label="name" sort="auto"/>
-                        <listheader label="gender" sort="auto"/>
-                        <listheader label="age" sort="auto"/>
-                      </listhead>
-                      <listitem>
-                        <listcell label="Mary"/>
-                        <listcell label="FEMALE"/>
-                        <listcell label="23"/>
-                      </listitem>
-                      <listitem>
-                        <listcell label="John"/>
-                        <listcell label="MALE"/>
-                        <listcell label="12"/>
-                      </listitem>
-                      <listitem>
-                        <listcell label="Jane"/>
-                        <listcell label="FEMALE"/>
-                        <listcell label="33"/>
-                      </listitem>
-                      <listitem>
-                        <listcell label="Henry"/>
-                        <listcell label="MALE"/>
-                        <listcell label="44"/>
-                      </listitem>
-                      <listfoot>
-                        <listfooter span="2">
-                          <label value="This is footer1"/>
-                        </listfooter>
-                        <listfooter>
-                          <label value="This is footer2"/>
-                        </listfooter>
-                      </listfoot>
-                    </listbox>
-                    <button label="hide/show name column" onClick='name.setVisible(!name.isVisible())'/>
-                  </zk>
-"""
-    runZTL(zscript,
-      () => {
-        click(jq("@button"))
-        waitResponse()
-        val hdfaker = jq(jq(".z-listheader:contains(name)").toWidget().$n("hdfaker"))
-        verifyEquals(hdfaker.css("visibility"), "hidden")
+    runZTL(() => {
+        val footer1 = jq("@listfooter:eq(0)")
+        val ft1Width = footer1.width()
 
         click(jq("@button"))
         waitResponse()
-        verifyNotEquals(hdfaker.css("visibility"), "hidden")
-      })
+        verifyTrue(footer1.width() < ft1Width)
+        verifyTrue(footer1.isVisible)
+
+        click(jq("@button"))
+        waitResponse()
+        verifyEquals(ft1Width, footer1.width())
+    })
 
   }
 }
---
+++ zstl/test/org/zkoss/zktest/test2/B86/B86_ZK_4011Test.scala
@@ -0,0 +1,37 @@
+package org.zkoss.zktest.test2.B86
+
+import org.junit.Test
+import org.zkoss.zstl.ZTL4ScalaTestCase
+import org.zkoss.ztl.annotation.Tags
+import org.zkoss.ztl.unit.JQuery
+
+/**
+  * @author rudyhuang
+  */
+@Tags(tags = "B86-ZK-4011.zul")
+class B86_ZK_4011Test extends ZTL4ScalaTestCase {
+  @Test
+  def test(): Unit =  {
+    runZTL(() => {
+      // The width of column 1, 4, 6 and Aux 3 should be nearly 0
+      verifyTolerant(0, getWidth(jq("@cell:eq(0)")), 1)
+      verifyTolerant(0, getWidth(jq("@cell:eq(3)")), 1)
+      verifyTolerant(0, getWidth(jq("@cell:eq(5)")), 1)
+      verifyTolerant(0, getWidth(jq("@auxheader:eq(2)")), 1)
+
+      click(jq("@button:contains(switch 1 visible)"))
+      waitResponse()
+      verifyTrue(getWidth(jq("@cell:eq(0)")) > 1)
+
+      click(jq("@button:contains(switch 6 visible)"))
+      waitResponse()
+      verifyTrue(getWidth(jq("@cell:eq(5)")) > 1)
+      verifyTrue(getWidth(jq("@auxheader:eq(2)")) > 1)
+    })
+  }
+
+  private def getWidth(elem: JQuery): Int = {
+    // Use clientWidth instead of width() to avoid negative bug https://stackoverflow.com/q/22832719
+    elem.toElement.attr("clientWidth").toInt
+  }
+}
