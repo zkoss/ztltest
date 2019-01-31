664a89e0c19009ed5465478dd60cb7c415d030a1
Charles Qiu <charlesqiu@potix.com>
2019-01-31 16:15:55 +0800

    Refine ZK-3626 test case

--- zstl/test/org/zkoss/zktest/test2/B85/B85_ZK_3626Test.scala
+++ zstl/test/org/zkoss/zktest/test2/B85/B85_ZK_3626Test.scala
@@ -28,23 +28,12 @@ class B85_ZK_3626Test extends ZTL4ScalaTestCase {
         evalScript(jq(".z-tabbox-down-scroll") + ".click()")
         waitResponse()
       }
-      var manubarBodyWidth = jq(".z-menubar-body").width()
-      var manubarBodyLeft = jq(".z-menubar-body").offsetLeft()
-      var menu8Left = jq(jq(".z-menubar").toWidget().lastChild()).offsetLeft()
-      var menuItemWidth = jq(jq(".z-menubar").toWidget().lastChild()).width()
-      verifyTrue(manubarBodyLeft + manubarBodyWidth + 2 > (menu8Left + menuItemWidth))
-
-      var tabsWidth = jq(".z-tabs:eq(0)").width()
-      var tabsLeft =  jq(".z-tabs:eq(0)").offsetLeft()
-      var tab8Width = jq(jq(".z-tabs:eq(0)").toWidget().lastChild()).width()
-      var tab8Left =  jq(jq(".z-tabs:eq(0)").toWidget().lastChild()).offsetLeft()
-      verifyTrue(tabsLeft + tabsWidth + 2 > (tab8Left + tab8Width))
-
-      var tabsHeight1 = jq(".z-tabs:eq(1)").height()
-      var tabsTop1 =  jq(".z-tabs:eq(1)").offsetTop()
-      var tab8Height1 = jq(jq(".z-tabs:eq(1)").toWidget().lastChild).height()
-      var tab8Top1 =  jq(jq(".z-tabs:eq(1)").toWidget().lastChild).offsetTop()
-      verifyTrue(tabsTop1+ tabsHeight1 + 2 > (tab8Top1 + tab8Height1))
+      click(jq(".z-button"))
+      waitResponse()
+      val result = getZKLog.split("\n")
+      for (i <- 0 to 2) {
+        verifyEquals("true", result(i))
+      }
       evalScript("document.body.style.zoom = '1'")
     })
   }
