af3cebd144409c6b2ab0312e7f20b199e41b95a1
leonlee <leonlee@potix.com>
2019-11-11 14:17:23 +0800

    ZK-4407: Unexpected scrollRestoration when chrome loading the page

---
+++ zstl/test/org/zkoss/zktest/test2/B90/B90_ZK_4407Test.scala
@@ -0,0 +1,22 @@
+package org.zkoss.zktest.test2.B90
+
+import org.junit.Test
+import org.zkoss.zstl.ZTL4ScalaTestCase
+import org.zkoss.ztl.annotation.{IgnoreBrowsers, Tags}
+
+@Tags(tags = "B90-ZK-4407.zul")
+@IgnoreBrowsers("ff,edge,ie11,ie10,ie9")
+class B90_ZK_4407Test extends ZTL4ScalaTestCase {
+  @Test
+  def test(): Unit =  {
+    runZTL(() => {
+      evalScript("window.scroll(0, 0)")
+      refresh()
+      waitResponse()
+      verifyNotEquals(0, jq(if (isSafari()) "body" else "html").scrollTop())
+      click(jq("@button"))
+      waitResponse()
+      verifyEquals("history.scrollRestoration: auto", getZKLog())
+    })
+  }
+}
