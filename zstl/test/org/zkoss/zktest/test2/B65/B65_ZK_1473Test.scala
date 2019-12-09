8e170946392acd35a7cdb22c7cb3a4479d85b5b0
Rudy Huang <rudyhuang@potix.com>
2019-12-09 14:58:51 +0800

    refine B65_ZK_1473Test

--- zstl/test/org/zkoss/zktest/test2/B65/B65_ZK_1473Test.scala
+++ zstl/test/org/zkoss/zktest/test2/B65/B65_ZK_1473Test.scala
@@ -10,39 +10,36 @@ class B65_ZK_1473Test extends ZTL4ScalaTestCase {
 
   @Test
   def testClick() = {
-    val zscript =
-      """<zk>
-        <zscript><![CDATA[
-	org.zkoss.zul.ListModel m = new org.zkoss.zktest.test2.grid.FakeListModel(300);
-	]]></zscript>
-        <label multiline="true">
-          1. Click "Option 0" in Listbox.
-	2. Hold "down" key by keyboard to scroll down Listbox, should not see it jump back to top.
-        </label>
-        <window width="300px" height="300px">
-          <listbox model="${m}" vflex="1">
-            <custom-attributes org.zkoss.zul.listbox.rod="false"/>
-            <listhead>
-              <listheader label="Listbox"/>
-            </listhead>
-            <template name="model">
-              <listitem label="${each}"/>
-            </template>
-          </listbox>
-        </window>
-      </zk>"""
-
-    runZTL(zscript,
+    runZTL(
       () => {
         val opt = jq(".z-listitem:contains(Option 0)")
         click(opt)
         waitResponse()
 
+        val a = widget("@listbox").$n("a")
+        sendKeys(a, Keys.PAGE_DOWN)
+        sleep(200)
+        sendKeys(a, Keys.PAGE_DOWN)
+        sleep(200)
+        sendKeys(a, Keys.PAGE_DOWN)
+        sleep(200)
+        sendKeys(a, Keys.PAGE_DOWN)
+        sleep(200)
+        sendKeys(a, Keys.PAGE_DOWN)
+        sleep(200)
+        sendKeys(a, Keys.PAGE_DOWN)
+        sleep(200)
+        sendKeys(a, Keys.PAGE_DOWN)
+        sleep(200)
+        sendKeys(a, Keys.PAGE_DOWN)
+        sleep(200)
+        sendKeys(a, Keys.PAGE_DOWN)
+        sleep(200)
+        sendKeys(a, Keys.PAGE_DOWN)
+        waitResponse()
+
         val seld = jq(".z-listitem-selected")
-
-        sendKeys(seld, Keys.DOWN)
         verifyNotEquals("should not see it jump back to top.", seld.html(), opt.html())
-
       })
 
   }
