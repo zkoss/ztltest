e7517f3632f6d0e9ce2e8a5a5484f0d09807f5be
leonlee <leonlee@potix.com>
2020-03-25 17:02:40 +0800

    refine test cases for select 2013 issue

--- zstl/test/org/zkoss/zktest/test2/F65/F65_ZK_1825Test.scala
+++ zstl/test/org/zkoss/zktest/test2/F65/F65_ZK_1825Test.scala
@@ -9,35 +9,7 @@ class F65_ZK_1825Test extends ZTL4ScalaTestCase {
 
   @Test
   def testClick() = {
-    val zscript =
-      """<?xml version="1.0" encoding="UTF-8"?>
-
-<!--
-F70-ZK-1825.zul
-
-	Purpose:
-		
-	Description:
-		
-	History:
-		Mon, Jun 24, 2013 11:13:08 AM, Created by jumperchen
-
-Copyright (C) 2013 Potix Corporation. All Rights Reserved.
-
--->
-<zk>
-	1. Please select a date in the datebox.
-	<separator/>
-	2. Change the time in the timebox.
-	<separator/>
-	3. Click the "show timebox value", the result should be the value of datebox + timebox.	It shouldn't be reset to today.
-	<datebox onChange="tb.value = self.value;"/>
-	<timebox id="tb"/>
-	<button label="show timebox value" onClick="alert(tb.value);"/>
-</zk>
-"""
-    runZTL(zscript,
-      () => {
+    runZTL(() => {
         val db = jq(".z-datebox").toWidget()
         val tb = jq(".z-timebox").toWidget()
         click(db.$n("btn"))
@@ -45,6 +17,10 @@ Copyright (C) 2013 Potix Corporation. All Rights Reserved.
 
         val calendar0 = jq(".z-calendar:eq(0)")
         click(calendar0.find(".z-calendar-title .z-calendar-text:eq(1)"))
+        waitResponse(true)
+        click(calendar0.find(".z-calendar-title .z-calendar-text:eq(0)"))
+        waitResponse(true)
+        click(calendar0.find(".z-calendar-body td:contains(2010-2019)"))
         waitResponse(true)
         click(calendar0.find(".z-calendar-body td:contains(2013)"))
         waitResponse(true)
