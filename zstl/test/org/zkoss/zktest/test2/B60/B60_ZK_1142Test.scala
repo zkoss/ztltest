6b9da255db7acc1326a6e2e5817bce847a3e4082
Rudy Huang <rudyhuang@potix.com>
2020-04-07 15:49:28 +0800

    refine B60_ZK_1142Test

--- zstl/test/org/zkoss/zktest/test2/B60/B60_ZK_1142Test.scala
+++ zstl/test/org/zkoss/zktest/test2/B60/B60_ZK_1142Test.scala
@@ -6,42 +6,12 @@ import org.zkoss.ztl.annotation.Tags
 
 @Tags(tags = "B60-ZK-1142.zul")
 class B60_ZK_1142Test extends ZTL4ScalaTestCase {
-
   @Test
-  def testClick() = {
-    val zscript =
-      """
-    	<zk>
-		    <window id="win_main" title="Hello IPad!!" border="normal" width="100%">
-		      <vlayout>
-		        <label>1. Try selecting New >> Document/Spreadsheet/Presentation.</label>
-		        <label>2. You should see alert message displayed</label>
-		      </vlayout>
-		      <menubar id="menubar" width="100%">
-		        <menu label="File">
-		          <menupopup>
-		            <menu label="New" style="cursor :pointer">
-		              <menupopup id="m" style="cursor :pointer">
-		                <menuitem label="Document" style="cursor :pointer" onClick="alert(self.label)"/>
-		                <menuitem label="Spreadsheet" style="cursor:pointer" onClick="alert(self.label)"/>
-		                <menuitem label="Presentation" style="cursor:pointer" onClick="alert(self.label)"/>
-		              </menupopup>
-		            </menu>
-		            <menuitem label="Open.." onClick="alert(self.label)"/>
-		            <menuseparator></menuseparator>
-		            <menuitem label="Save" onClick="alert(self.label)"/>
-		          </menupopup>
-		        </menu>
-		        <menuitem label="ZK Web Framework" onClick="alert(self.label)"/>
-		      </menubar>
-		    </window>
-    	</zk>
-    """
-
-    runZTL(zscript,
+  def testClick(): Unit = {
+    runZTL(
       () => {
         val newMenu = jq(".z-menu:contains(New)")
-        val strs = List("Document", "Spreadsheet")
+        val strs = List("Document", "Spreadsheet", "Presentation")
         for (str <- strs) {
           click(jq("@menu:contains(File)"))
           waitResponse()
@@ -49,14 +19,12 @@ class B60_ZK_1142Test extends ZTL4ScalaTestCase {
           waitResponse()
           mouseOver(newMenu)
           waitResponse()
-          click(jq("@menuitem:contains(" + str + ")"))
+          click(jq(s"@menuitem:contains($str)"))
           waitResponse()
           verifyEquals("should see alert message displayed", jq(".z-messagebox-window @label").text(), str)
           click(jq("@button"))
           waitResponse()
         }
-
       })
-
   }
 }
