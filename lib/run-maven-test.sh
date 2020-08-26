14dde9e58cfa1a036bde280021e2ad49491d3228
DevChu <marchuqq@gmail.com>
2020-08-26 09:41:14 +0800

    Add report suffix support (for A11y/NoA11y)

--- lib/run-maven-test.sh
+++ lib/run-maven-test.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-echo "params=\"$1\" \"$2\" \"$3\" \"$4\""
-mvn surefire:test -Dreport.dir="$1" -Dtest.dir="$2" -Dbrowser="$3" -Dopenonce=$4
+echo "params=\"$1\" \"$2\" \"$3\" \"$4\" \"$5\""
+mvn surefire:test -Dreport.dir="$1" -Dtest.dir="$2" -Dbrowser="$3" -Dopenonce=$4 -Dsurefire.reportNameSuffix="$5"
