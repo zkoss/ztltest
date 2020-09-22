f85e4d3789a420c30bdff7e13ab48e3b3f230bc6
Rudy Huang <rudyhuang@potix.com>
2020-09-22 12:03:55 +0800

    Convert B30_1997828Test, B30_2562880Test into ZATS Refine B65_ZK_1612Test

--- zstl/test/org/zkoss/zktest/test2/B65/B65_ZK_1612Test.scala
+++ zstl/test/org/zkoss/zktest/test2/B65/B65_ZK_1612Test.scala
@@ -7,51 +7,7 @@ import org.zkoss.ztl.annotation.Tags
 class B65_ZK_1612Test extends ZTL4ScalaTestCase {
 
   def testClick() = {
-    val zscript =
-      """
-<zk>
-
-<zscript><![CDATA[
-void changeTitle(org.zkoss.zul.Panel thePanel)
-{
-   thePanel.setTitle("Dropped!");
-}
-
-   ]]>
-    </zscript>  
-<label multiline="true">
-1. Please click the "Button Image"
-2. You should see a dialog and close it.
-3. Please drag the "Button Image" into the window of the right side panel.
-4. You should see the title of the right side panel is changed.
-</label>
-    <window width="800px" height="800px" border="normal" title="test">
-    <grid>
-    <rows>
-    <row>
-    <panel id="panel1" title="PANEL1" droppable="ABC" onDrop="changeTitle(panel1);">
-    <panelchildren>
-    <window width="200px" height="200px" border="normal" draggable="ABC">
-    <toolbarbutton image="/img/button.png" onClick="alert(1)">
-
-    </toolbarbutton>
-    </window>
-    </panelchildren>
-    </panel>
-
-    <panel id="panel2"  title="PANEL2" droppable="ABC" onDrop="changeTitle(panel2);">
-    <panelchildren>
-    <window width="200px" height="200px" border="normal" draggable="ABC" />
-    </panelchildren>
-    </panel>
-    </row>
-    </rows>
-    </grid>
-    </window>
-
-</zk>
-"""
-    runZTL(zscript,
+    runZTL(
       () => {
         val img = jq("img[src*=button]")
 
