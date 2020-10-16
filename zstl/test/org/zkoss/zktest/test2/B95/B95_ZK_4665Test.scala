a454182bce3499bd6a5d316faee35c256d2b6376
DevChu <marchuqq@gmail.com>
2020-10-16 18:32:34 +0800

    add ZK-4665 test case

---
+++ zstl/test/org/zkoss/zktest/test2/B95/B95_ZK_4665Test.scala
@@ -0,0 +1,16 @@
+package org.zkoss.zktest.test2.B95
+
+import org.junit.Test
+import org.zkoss.zstl.ZTL4ScalaTestCase
+import org.zkoss.ztl.annotation.IgnoreBrowsers
+
+@IgnoreBrowsers("chrome,safari,ie11,ie10,ie9,edge-chromium")
+class B95_ZK_4665Test extends ZTL4ScalaTestCase {
+  @Test
+  def test() = {
+    runZTL(() => {
+      waitResponse()
+      verifyEquals(jq("$p1").offsetTop(), (jq("$p2").offsetTop()))
+    })
+  }
+}
