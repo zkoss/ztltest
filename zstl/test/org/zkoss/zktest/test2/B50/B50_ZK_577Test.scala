62a1ad4236f26eb842d69f0279308b657244720f
learningstud <gordonhsu@potix.com>
2022-05-13 17:30:49 +0800

    Fix catch-all import in Scala files

--- zstl/test/org/zkoss/zktest/test2/B50/B50_ZK_577Test.scala
+++ zstl/test/org/zkoss/zktest/test2/B50/B50_ZK_577Test.scala
@@ -18,9 +18,8 @@ package org.zkoss.zktest.test2.B50
 
 import org.junit.Test
 import org.zkoss.zstl.ZTL4ScalaTestCase
-import org.zkoss.ztl._
+import org.zkoss.ztl.unit.Widget
 import org.zkoss.ztl.annotation.Tags
-import org.zkoss.ztl.unit._
 
 /**
   * A test class for bug ZK-577
